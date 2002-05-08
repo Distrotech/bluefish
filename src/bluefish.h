d8834b25480fe1f841222ea663e686de9ede1fb4
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2002-05-08 21:55:23 +0000

    forgot some files
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@5 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/bluefish.h
+++ src/bluefish.h
@@ -18,3 +18,87 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+#ifndef __BLUEFISH_H_
+#define __BLUEFISH_H_
+
+/*********************/
+/* undo/redo structs */
+/*********************/
+typedef enum {
+	UndoDelete = 1, UndoInsert
+} undo_op_t;
+
+typedef struct {
+	GList *entries;	/* the list of entries that should be undone in one action */
+	gint changed;		/* doc changed status at this undo node */
+} unregroup_t;
+
+typedef struct {
+	GList *first;
+	GList *last;
+	unregroup_t *current;
+	gint num_groups;
+	GList *redofirst;
+} unre_t;
+
+/************************/
+/* highlighting structs */
+/************************/
+
+typedef struct {
+	gchar *spat1;
+	gchar *spat2;
+	gchar *spat3;
+	gchar *name;
+	gchar *parentname;
+	regex_t rpat1;
+	regex_t rpat2;
+	regex_t rpat3;
+	gint mode;
+	gint style;
+	GList *childs;
+	gboolean need_subpatterns;
+	GtkTextTag *tag;
+} Tpattern;
+
+typedef struct {
+	gchar *type;
+	gchar **extensions;
+	gchar *update_chars;
+	GList *highlightlist;
+	GtkTextTagTable *tagtable;
+} Thighlightset;
+
+/*******************/
+/* document struct */
+/*******************/
+
+typedef struct {
+	gchar *filename;
+	gint modified;
+	time_t mtime; /* from stat() */
+	uid_t owner_uid; /* user ID of owner */
+	gid_t owner_gid; /* group ID of owner */
+	mode_t mode; /* mode field in stat() format so any stat macro's will work*/
+	gint is_symlink; /* file is a symbolic link */
+	unsigned del_txt_id;
+	unsigned ins_txt_id;
+	unre_t unre;
+	GtkWidget *view;
+	GtkWidget *tab_label;
+	GtkTextBuffer *buffer;
+	gint last_rbutton_event; /* index of last 3rd button click */
+	Thighlightset *hl; /* highlighting set to use for this document */
+} Tdocument;
+
+typedef struct {
+	Tdocument *current_document;
+	GList *documentlist;
+	GList *hlsetlist;
+	GtkWidget *main_window;
+	GtkWidget *notebook;
+} Tmain;
+
+extern Tmain *main_v;
+
+#endif /* __BLUEFISH_H_ */
