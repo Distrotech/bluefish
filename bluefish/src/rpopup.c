776ec59790ffa8c5391e657b950704c73aa9d081
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2002-05-19 14:19:11 +0000

    tag detection is working
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@30 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/rpopup.c
+++ bluefish/src/rpopup.c
@@ -5,11 +5,11 @@
 typedef struct {
 	guint32 findchar;
 	guint32 prevchar;
+	guint32 ignore_if_prevchar;
 } Tin_html_tag;
 
 static gboolean iter_char_search_lcb(gunichar ch,Tin_html_tag *iht){
-	DEBUG_MSG("iter_char_search_lcb, called for character %d\n", ch);
-	if (ch==iht->findchar && iht->prevchar != 63) { /* 63 = ? */
+	if (ch==iht->findchar && iht->prevchar != iht->ignore_if_prevchar) {
 		return TRUE;
 	}
 	iht->prevchar = ch;
@@ -17,15 +17,16 @@ static gboolean iter_char_search_lcb(gunichar ch,Tin_html_tag *iht){
 }
 
 
-gboolean doc_bevent_in_html_tag(Tdocument *doc, GdkEventButton *bevent) {
+gboolean doc_bevent_in_html_tag(Tdocument *doc, GdkEventButton *bevent
+		, gint *tag_so, gint *tag_eo) {
 	gint xpos, ypos;
 	GtkTextWindowType wintype;
 	GtkTextIter iter;
 
 	wintype = gtk_text_view_get_window_type(GTK_TEXT_VIEW(doc->view), doc->view->window);
-	gtk_text_view_window_to_buffer_coords(GTK_TEXT_VIEW(doc->view), wintype,
-											  bevent->x, bevent->y,
-											  &xpos, &ypos);
+	gtk_text_view_window_to_buffer_coords(GTK_TEXT_VIEW(doc->view), wintype,bevent->x, bevent->y,
+					  &xpos, &ypos);
+	*tag_so = *tag_eo = -1;	
 	g_print("doc_bevent_in_html_tag, xpos=%d, ypos=%d\n", xpos, ypos);
 	gtk_text_view_get_iter_at_location(GTK_TEXT_VIEW(doc->view), &iter, xpos, ypos);
 	{
@@ -36,17 +37,46 @@ gboolean doc_bevent_in_html_tag(Tdocument *doc, GdkEventButton *bevent) {
 
 		/* backward search for tag start */
 		iht.findchar = 62;/* 62 = > */
+		iht.prevchar = 10; /* \n */
+		iht.ignore_if_prevchar = 0;
 		gtfound = gtk_text_iter_backward_find_char(&gtiter,iter_char_search_lcb,
                                              &iht,NULL);
 		/* perhaps we should limit the search fto 50 charcters or so */
 		iht.findchar = 60;/* 60 = < */
+		iht.prevchar = 10; /* \n */
+		iht.ignore_if_prevchar = 63 /* ? */;
 		ltfound = gtk_text_iter_backward_find_char(&ltiter,iter_char_search_lcb,
                                              &iht,NULL);
 		if ((ltfound && gtfound && gtk_text_iter_compare(&ltiter,&gtiter) > 0)
 				|| (ltfound && !gtfound)) {
-			DEBUG_MSG("a tag is started on the left side\n");
+			*tag_so = gtk_text_iter_get_offset(&ltiter);
+			DEBUG_MSG("a tag is started on the left side at %d\n",*tag_so);
 		} else {
 			DEBUG_MSG("no tag start found on the left side\n");
 		}
+
+		if (*tag_so >=0) {
+			/* forward search for end tag */
+			iht.findchar = 62;/* 62 = > */
+			iht.prevchar = 10; /* \n */
+			iht.ignore_if_prevchar = 63 /* ? */;
+			gtfound = gtk_text_iter_forward_find_char(&gtiter,iter_char_search_lcb,
+                                             &iht,NULL);
+			/* perhaps we should limit the search fto 50 charcters or so */
+			iht.findchar = 60;/* 60 = < */
+			iht.prevchar = 10; /* \n */
+			iht.ignore_if_prevchar = 0;
+			ltfound = gtk_text_iter_forward_find_char(&ltiter,iter_char_search_lcb,
+                                             &iht,NULL);
+			if ((ltfound && gtfound && gtk_text_iter_compare(&ltiter,&gtiter) > 0)
+				|| (gtfound && !ltfound)) {
+				*tag_eo = gtk_text_iter_get_offset(&gtiter);
+				DEBUG_MSG("a tag is ended on the right side at %d\n",*tag_eo);
+				return TRUE;
+			} else {
+				DEBUG_MSG("no tag end found on the right side\n");
+			}
+		}
 	}
+	return FALSE;
 }
--- bluefish/src/rpopup.h
+++ bluefish/src/rpopup.h
@@ -1,1 +1,2 @@
-gboolean doc_bevent_in_html_tag(Tdocument *doc, GdkEventButton *bevent);
+gboolean doc_bevent_in_html_tag(Tdocument *doc, GdkEventButton *bevent
+		, gint *tag_so, gint *tag_eo);
