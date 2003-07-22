51c8a83ba31c30ed264639b6f8cbd6fb9f322f6a
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2003-07-22 09:49:29 +0000

    gui.c is now compiling
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@663 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/outputbox.h
+++ bluefish/src/outputbox.h
@@ -1,3 +1,3 @@
-void init_output_box(GtkWidget *vbox);
+void init_output_box(Tbfwin *bfwin,GtkWidget *vbox);
 void outputbox(gchar *pattern, gint file_subpat, gint line_subpat, gint output_subpat, gchar *command, gboolean show_all_output);
 
