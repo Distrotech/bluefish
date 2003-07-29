40daf5531ba0f10a9915e95458b904c7e366ccbb
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2003-07-29 12:51:32 +0000

    fixed a segfault in the right-click-opup-tag-parsing code
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@698 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/rpopup.c
+++ src/rpopup.c
@@ -168,7 +168,7 @@ typedef struct {
 	void (*func) ();
 }Ttagdialog;
 
-static void parse_tagstring(gchar * tagstring, gint pos, gint end)
+static void parse_tagstring(Tbfwin *bfwin, gchar * tagstring, gint pos, gint end)
 {
 	GList *tmplist = NULL;
 	gchar *tmpstring, *item, *value;
@@ -303,7 +303,7 @@ static void parse_tagstring(gchar * tagstring, gint pos, gint end)
 	gint i, numitems = (sizeof(tagdia)/sizeof(Ttagdialog));
 		for (i=0;i<numitems;i++) {
 			if (strcmp(tmpstring, tagdia[i].tag) == 0) {
-				tagdia[i].func(NULL, tag_popup);
+				tagdia[i].func(bfwin, tag_popup);
 				break;
 			}
 		}
@@ -359,7 +359,7 @@ void rpopup_edit_tag_cb(GtkMenuItem *menuitem,Tdocument *doc) {
 		/* we do not need the < and > chars so we cut those */
 		text = doc_get_chars(doc, rec_tag.tag_so+1, rec_tag.tag_eo - 1);
 		DEBUG_MSG("rpopup_edit_tag_cb, about to parse %s\n", text);
-		parse_tagstring(text, rec_tag.tag_so, rec_tag.tag_eo);
+		parse_tagstring(doc->bfwin, text, rec_tag.tag_so, rec_tag.tag_eo);
 		g_free(text);
 	} else {
 		DEBUG_MSG("rpopup_edit_tag_cb, no tag search known!!\n");
