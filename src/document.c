3fb3a30fd267fdeb5f7576701d27b9da34691883
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2003-10-01 08:33:36 +0000

    indenting the current line without a selection is functional now
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@825 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/document.c
+++ src/document.c
@@ -3497,8 +3497,42 @@ void doc_indent_selection(Tdocument *doc, gboolean unindent) {
 		doc_bind_signals(doc);
 		doc_set_modified(doc, 1);
 	} else {
-		DEBUG_MSG("doc_indent_selection, put a message on the statusbar that we want a selection..\n");
-		statusbar_message(BFWIN(doc->bfwin),_("No selection to indent"), 2000);
+		/* there is no selection, work on the current line */
+		GtkTextIter iter;
+		gtk_text_buffer_get_iter_at_mark(doc->buffer,&iter,gtk_text_buffer_get_insert(doc->buffer));
+		gtk_text_iter_set_line_offset(&iter,0);
+		if (unindent) {
+			gint deletelen = 0;
+			gchar *tmpstr, *tmp2str;
+			GtkTextIter itend = iter;
+			gtk_text_iter_forward_chars(&itend,main_v->props.editor_tab_width);
+			tmpstr = gtk_text_buffer_get_text(doc->buffer,&iter,&itend,FALSE);
+			tmp2str = bf_str_repeat(" ", main_v->props.editor_tab_width);
+			if (tmpstr[0] == '\t') {
+				deletelen = 1;
+			} else if (tmpstr && strncmp(tmpstr,tmp2str,main_v->props.editor_tab_width)==0) {
+				deletelen = main_v->props.editor_tab_width;
+			}
+			g_free(tmpstr);
+			g_free(tmp2str);
+			if (deletelen) {
+				itend = iter;
+				gtk_text_iter_forward_chars(&itend,deletelen);
+				gtk_text_buffer_delete(doc->buffer,&iter,&itend);
+			}
+		} else { /* indent */
+			gchar *indentstring;
+			gint indentlen;
+			if (main_v->props.editor_indent_wspaces) {
+				indentstring = bf_str_repeat(" ", main_v->props.editor_tab_width);
+				indentlen = main_v->props.editor_tab_width;
+			} else {
+				indentstring = g_strdup("\t");
+				indentlen=1;
+			}
+			gtk_text_buffer_insert(doc->buffer,&iter,indentstring,indentlen);
+			g_free(indentstring);
+		}
 	}
 }
 
