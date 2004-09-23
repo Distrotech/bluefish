8d727446aa59867c119e2ba659dd24f808639f16
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2004-09-23 12:30:39 +0000

    Workaround for an input bug reported by Timothy Joko-Veltman <tim-oss@indigopixel.com> when using modkey+ENTER as a keyboard shortcut
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@1349 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/document.c
+++ bluefish/src/document.c
@@ -1816,7 +1816,7 @@ static gboolean doc_view_key_release_lcb(GtkWidget *widget,GdkEventKey *kevent,T
 			}
 #endif
 		}
-	} else if (kevent->keyval == GDK_Return || kevent->keyval == GDK_KP_Enter) {
+	} else if ((kevent->keyval == GDK_Return || kevent->keyval == GDK_KP_Enter) && !(kevent->state)) {
 		if (main_v->props.autoindent) {
 			gchar *string, *indenting;
 			GtkTextMark* imark;
