377d774f78f19e0b4a25f58954c5df4b1bcde054
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2004-10-04 20:43:16 +0000

    implemented highlighting after paste
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@1368 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/bluefish.h
+++ src/bluefish.h
@@ -133,7 +133,7 @@ typedef struct {
 	GtkWidget *tab_eventbox;
 	GtkWidget *tab_menu;
 	GtkTextBuffer *buffer;
-	gboolean in_paste_operation;
+	gpointer paste_operation;
 	gint last_rbutton_event; /* index of last 3rd button click */
 	Tfiletype *hl; /* filetype & highlighting set to use for this document */
 	gint need_highlighting; /* if you open 10+ documents you don't need immediate highlighting, just set this var, and notebook_switch() will trigger the actual highlighting when needed */
