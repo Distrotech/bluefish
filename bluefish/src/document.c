e698f34436956311be4f8ad5486c5edc0b21c3e9
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2005-01-23 19:37:04 +0000

    improving the right-click menu in the new filebrowser
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@1803 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/document.c
+++ bluefish/src/document.c
@@ -1,7 +1,7 @@
 /* Bluefish HTML Editor
  * document.c - the document
  *
- * Copyright (C) 1998-2004 Olivier Sessink
+ * Copyright (C) 1998-2005 Olivier Sessink
  * Copyright (C) 1998 Chris Mazuc
  * some additions Copyright (C) 2004 Eugene Morenko(More)
  *
