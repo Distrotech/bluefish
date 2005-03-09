0500136350aa642d48c5e6c7377045dfef2159f4
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2005-03-09 12:18:46 +0000

    opening a project from the filebrowser works again
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@2070 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/stringlist.c
+++ src/stringlist.c
@@ -751,35 +751,36 @@ GList *duplicate_arraylist(GList *arraylist) {
  * gets a stringlist from a file
  */
 GList *get_list(const gchar * filename, GList * which_list, gboolean is_arraylist) {
+	FILE *fd;
+	gchar *tmpbuf, *tmpfilename;
+
 	DEBUG_MSG("get_stringlist, started with filename=%s\n", filename);
 	if (filename == NULL) {
 		return NULL;
 	}
-	if (file_exists_and_readable(filename)) {
-		FILE *fd;
-		gchar *tmpbuf;
-		DEBUG_MSG("get_stringlist, opening %s\n", filename);
-		fd = fopen(filename, "r");
-		if (fd == NULL) {
-			return NULL;
-		}
-		tmpbuf = g_malloc(STRING_MAX_SIZE);
-		while (fgets(tmpbuf, STRING_MAX_SIZE, fd) != NULL) {
-			gchar *tempstr;
-			tmpbuf = trunc_on_char(tmpbuf, '\n');
-			tempstr = g_strdup(tmpbuf);
-			if (is_arraylist) {
-				gchar **temparr = string_to_array(tempstr);
-				which_list = g_list_append(which_list, temparr);
-				g_free(tempstr);
-			} else {
-				DEBUG_MSG("get_list, adding string \"%s\" to the stringlist=%p\n", tempstr, which_list);
-				which_list = g_list_append(which_list, tempstr);
-			}
-		}
-		fclose(fd);
-		g_free(tmpbuf);
+	tmpfilename = g_strdup(filename+((strncmp(filename, "file://",7)==0)?7:0));
+	DEBUG_MSG("get_stringlist, opening %s\n", tmpfilename);
+	fd = fopen(tmpfilename, "r");
+	g_free(tmpfilename);
+	if (fd == NULL) {
+		return NULL;
 	}
+	tmpbuf = g_malloc(STRING_MAX_SIZE);
+	while (fgets(tmpbuf, STRING_MAX_SIZE, fd) != NULL) {
+		gchar *tempstr;
+		tmpbuf = trunc_on_char(tmpbuf, '\n');
+		tempstr = g_strdup(tmpbuf);
+		if (is_arraylist) {
+			gchar **temparr = string_to_array(tempstr);
+			which_list = g_list_append(which_list, temparr);
+			g_free(tempstr);
+		} else {
+			DEBUG_MSG("get_list, adding string \"%s\" to the stringlist=%p\n", tempstr, which_list);
+			which_list = g_list_append(which_list, tempstr);
+		}
+	}
+	fclose(fd);
+	g_free(tmpbuf);
 	return which_list;
 }
 #ifdef __GNUC__
