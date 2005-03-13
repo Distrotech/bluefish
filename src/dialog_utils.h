8c42712cee1fce9bad8a4d6433a6d2fbd6f0460f
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2005-03-13 23:41:58 +0000

    Some code changes for some work on the new html dialogs Try to limit line length to 120 chars
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@2084 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/dialog_utils.h
+++ src/dialog_utils.h
@@ -21,11 +21,27 @@
 #ifndef __DIALOG_UTILS_H_
 #define __DIALOG_UTILS_H_
 
-GtkWidget * dialog_label_with_alignment(const gchar *labeltext, GtkWidget *box);
-GtkWidget * dialog_table_in_vbox(gint rows, gint cols, gint borderWidth, GtkWidget *box, gboolean expand, gboolean fill, guint padding);
+void dialog_mnemonic_label_in_table(const gchar *labeltext,
+												GtkWidget *m_widget,
+												GtkWidget *table,
+												guint left_attach, guint right_attach, guint top_attach, guint bottom_attach);
+												
+GtkWidget * dialog_vbox_labeled(const gchar *labeltext, GtkWidget *box);
+
+GtkWidget * dialog_table_in_vbox(gint rows, gint cols, 
+											gint borderWidth, 
+											GtkWidget *box, 
+											gboolean expand, gboolean fill, guint padding);
+											
 GtkWidget * dialog_table_in_vbox_defaults(gint rows, gint cols, gint borderWidth, GtkWidget *box);
 
-void message_dialog_new(GtkWidget *parent, GtkMessageType type, GtkButtonsType button, const gchar *primaryText, const gchar *secondaryText);
-gint message_dialog_new_multi(GtkWidget *parent, GtkMessageType type, const gchar *buttons[], const gchar *primaryText, const gchar *secondaryText);
+void message_dialog_new(GtkWidget *parent, 
+								GtkMessageType type, 
+								GtkButtonsType button, 
+								const gchar *primaryText, const gchar *secondaryText);
+								
+gint message_dialog_new_multi(GtkWidget *parent, 
+										GtkMessageType type, const gchar *buttons[],
+										const gchar *primaryText, const gchar *secondaryText);
 
 #endif /* __DIALOG_UTILS_H_ */
