172b01985e22cd56356572f98bfe33d21572a5f3
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2005-05-03 12:48:37 +0000

    and more cleanups
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@2175 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/bf_lib.h
+++ bluefish/src/bf_lib.h
@@ -82,7 +82,7 @@ gchar *ending_slash(const gchar *dirname);
 gchar *path_get_dirname_with_ending_slash(const gchar *filename);
 gboolean file_exists_and_readable(const gchar * filename);
 gchar *return_first_existing_filename(const gchar * filename, ...);
-gboolean filename_test_extensions(gchar **extensions, gchar *filename);
+gboolean filename_test_extensions(gchar **extensions, const gchar *filename);
 gchar *bf_str_repeat(const gchar * str, gint number_of);
 gint get_int_from_string(gchar *string);
 gchar *create_secure_dir_return_filename();
