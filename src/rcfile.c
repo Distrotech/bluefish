7ddb9a5da41f12e23cfd936a81961480f83e1e8d
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2005-05-04 22:08:30 +0000

    added option to disable the splash screen
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@2182 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/rcfile.c
+++ src/rcfile.c
@@ -386,12 +386,12 @@ static GList *props_init_main(GList * config_rc)
 	init_prop_integer(&config_rc, &main_v->props.autoindent, "autoindent:", 1, TRUE);
 	init_prop_integer(&config_rc, &main_v->props.drop_at_drop_pos, "drop_at_drop_position:", 0, TRUE);
 	init_prop_integer(&config_rc, &main_v->props.link_management, "link_management:", 1, TRUE);
-	
 #ifdef WITH_MSG_QUEUE
 	init_prop_integer (&config_rc, &main_v->props.open_in_running_bluefish,"open_in_running_bluefish:",1, TRUE);
 #endif
-#ifdef HAVE_GNOME_VFS
 	init_prop_integer (&config_rc, &main_v->props.server_zope_compat,"server_zope_compat:", 0, TRUE); 
+#ifndef NOSPLASH
+	init_prop_integer(&config_rc, &main_v->props.show_splash_screen, "show_splash_screen:", 1, TRUE);
 #endif
 	return config_rc;
 }
