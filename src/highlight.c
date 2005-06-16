2ba0c79602e60e587c2861b9e562e49dca9f45d7
swida <swida@7640d186-872a-4c71-b7b2-a5696852cc35>
2005-06-16 22:06:55 +0000

    Modified to show tag attributes.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@2342 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/highlight.c
+++ src/highlight.c
@@ -194,8 +194,10 @@ void hl_tag_end (BfTextView * self, gchar * tagName, GtkTextIter * b_startIter,
 	
 	if ( gdk_color_parse("#C56819",&col) ) {
 		tag = gtk_text_tag_table_lookup(gtk_text_buffer_get_tag_table(buffer),"tagcolor");
-		if ( tag==NULL )
+		if ( tag==NULL ) {
 			tag = gtk_text_buffer_create_tag(buffer,"tagcolor","foreground-gdk",&col,NULL);
+			gtk_text_tag_set_priority(tag,0);
+		}	
 			gtk_text_buffer_apply_tag_by_name(buffer,"tagcolor",b_startIter,b_endIter);	
 			gtk_text_buffer_apply_tag_by_name(buffer,"tagcolor",e_startIter,e_endIter);	
 	}
@@ -211,8 +213,10 @@ void hl_tag_attr (BfTextView * self, gchar * attrName, gchar * attrValue,
 	
 	if ( gdk_color_parse("#000077",&col) ) {
 		tag = gtk_text_tag_table_lookup(gtk_text_buffer_get_tag_table(buffer),"attrcolor");
-		if ( tag==NULL )
+		if ( tag==NULL ) {
 			tag = gtk_text_buffer_create_tag(buffer,"attrcolor","foreground-gdk",&col,NULL);
+			gtk_text_tag_set_priority(tag,1);
+		}	
 			gtk_text_buffer_apply_tag_by_name(buffer,"attrcolor",n_startIter,n_endIter);	
 	}
 			   
