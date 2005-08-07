f80fefb3b7a0c3e5e985c5a1abc3aca68f08a9f7
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2005-08-07 11:53:17 +0000

    patch From: Mikael Hultgren <blomma@fastmail.fm>
    These patches replaces the use of g_strdown with g_utf8_strdown and
    g_strup with g_utf8_strup, this is againts cvs HEAD. They compile
    fine under gcc4, i havent been able to test under gcc3 or ealier. The
    program also runs fine with those 2 patches applied.
    
    
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@2462 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/plugin_htmlbar/rpopup.c
+++ bluefish/src/plugin_htmlbar/rpopup.c
@@ -319,7 +319,7 @@ static void parse_tagstring(Tbfwin *bfwin, gchar * tagstring, gint pos, gint end
 					item = g_strndup(&tmpstring[prevtag + 1], count - prevtag);
 					value = g_strdup("");
 				}
-				g_strdown(item);
+				g_utf8_strdown(item, strlen(item));
 				g_strstrip(item);
 				tag_item = g_malloc(sizeof(Ttagitem));
 				tag_item->item = item;
@@ -342,7 +342,7 @@ static void parse_tagstring(Tbfwin *bfwin, gchar * tagstring, gint pos, gint end
 
 	tmpstring = g_strdup(tagstring);
 	tmpstring = trunc_on_char(tmpstring, ' ');
-	g_strdown(tmpstring);
+	g_utf8_strdown(tmpstring, strlen(tmpstring));
 	/* identifying which tag we have */
 	DEBUG_MSG("parse_tagstring, searching for dialog for %s\n", tmpstring);
 	
