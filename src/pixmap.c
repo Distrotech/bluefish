1caa07b4eb2d14b4866fe53580af6875fb413534
swida <swida@7640d186-872a-4c71-b7b2-a5696852cc35>
2005-09-01 09:20:13 +0000

    Some changes in highlight preferences.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@2523 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/pixmap.c
+++ src/pixmap.c
@@ -68,8 +68,8 @@ static Tstockpixmap tsp[] = {
 	{1000, GTK_STOCK_FIND},
 	{1001, GTK_STOCK_FIND_AND_REPLACE},
 	{1002, GTK_STOCK_PREFERENCES},
-	{1003, GTK_STOCK_GO_BACK},
-	{1004,GTK_STOCK_GO_FORWARD}	
+	{1003, GTK_STOCK_GO_UP},
+	{1004,GTK_STOCK_GO_DOWN}	
 };
 
 GtkWidget *new_pixmap(gint type) {
