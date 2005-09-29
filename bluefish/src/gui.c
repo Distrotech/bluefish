b4c2f695b40b12d61a3f48ef4c02fef0ac25afc8
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2005-09-29 13:05:29 +0000

    setting encoding and filetype in menu again if scanner is enabled ( why was this disabled?)
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@2607 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/gui.c
+++ bluefish/src/gui.c
@@ -447,9 +447,9 @@ void gui_set_document_widgets(Tdocument *doc) {
 	setup_toggle_item(gtk_item_factory_from_widget(BFWIN(doc->bfwin)->menubar),"/Document/Show blocks", doc->blocksstate);
 	setup_toggle_item(gtk_item_factory_from_widget(BFWIN(doc->bfwin)->menubar),"/Document/Show symbols", doc->symstate);
 #endif	
-#ifndef USE_SCANNER	
+/*#ifndef USE_SCANNER	why did we not set the encoding and filetype with the scanner enabled????*/
 	menu_current_document_set_toggle_wo_activate(BFWIN(doc->bfwin),doc->hl, doc->encoding);
-#endif	
+/*#endif	*/
 }
 
 void gui_notebook_bind_signals(Tbfwin *bfwin) {
