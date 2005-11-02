df02ced06b4aa02a3618b7f879636c4f3c1764d9
dleidert <dleidert@7640d186-872a-4c71-b7b2-a5696852cc35>
2005-11-02 15:04:56 +0000

    First attempt to fix http://bugzilla.gnome.org/show_bug.cgi?id=319753 by adding some defaults, if "Link to Stylesheet" menu item is used. Small typo fix in embedok_lcb(). Samll fix for linkdialogok_lcb(), so the custom part of the link-dialog is included in output.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/branches/bluefish_1_0/bluefish@2679 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/gui.c
+++ src/gui.c
@@ -520,7 +520,7 @@ static void quicklist_clicked_lcb(GtkWidget *widget, Tbfwin *bfwin) {
 	quicklist_dialog(bfwin,NULL);
 }
 static void link_clicked_lcb(GtkWidget *widget, Tbfwin *bfwin) {
-	linkdialog_dialog(bfwin,NULL);
+	linkdialog_dialog(bfwin,NULL,linkdialog_mode_css);
 }
 static void formdialog_clicked_lcb(GtkWidget *widget, Tbfwin *bfwin) {
 	formdialog_dialog(bfwin,NULL);
