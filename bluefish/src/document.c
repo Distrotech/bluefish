681e7a81a92314394b0c0a78061f87b856b02633
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2006-01-22 05:43:00 +0000

    Fix a bug in smart cursor positioning at line end if the last line contained spaces at the end of the line.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@2837 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/document.c
+++ bluefish/src/document.c
@@ -1867,14 +1867,14 @@ static gboolean doc_view_key_press_lcb(GtkWidget *widget,GdkEventKey *kevent,Tdo
                             gtk_text_iter_forward_char (&iter);
                             
                 } else { /* (kevent->keyval == GDK_End) || (kevent->keyval == GDK_KP_End) */
-		            if (!gtk_text_iter_ends_line (&iter)) {
+		            if (!gtk_text_iter_ends_line (&iter))
 		                    gtk_text_iter_forward_to_line_end (&iter);
-		            }
+
 		            linestart = iter;
 
-		            while (g_unichar_isspace (gtk_text_iter_get_char (&iter)) && !gtk_text_iter_starts_line (&iter))
+		            while ((g_unichar_isspace (gtk_text_iter_get_char (&iter)) && !gtk_text_iter_starts_line (&iter)) || gtk_text_iter_is_end (&iter))
 			                gtk_text_iter_backward_char (&iter);
-			                
+
                     if (!gtk_text_iter_starts_line (&iter))
                         gtk_text_iter_forward_char (&iter);
                 }
