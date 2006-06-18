52875a6cabe3f3c5bb9bd467bb7e1d6302d298eb
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2006-06-18 05:32:39 +0000

    Add a header file for filetype.c
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@3024 7640d186-872a-4c71-b7b2-a5696852cc35

---
+++ src/filetype.h
@@ -0,0 +1,28 @@
+/* Bluefish HTML Editor
+ * filetype.h
+ *
+ * Copyright (C) 2006 Olivier Sessink
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __FILETYPE_H_
+#define __FILETYPE_H_
+
+#include "bluefish.h"
+
+Tfiletype *get_filetype_for_mime_type(const gchar *mime_type);
+
+#endif /* __FILETYPE_H_ */
