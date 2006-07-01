3f56d29f111bc73572561ee5f520ac9714f000ac
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2006-07-01 22:15:33 +0000

    start of the filefilter gui
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@3051 7640d186-872a-4c71-b7b2-a5696852cc35

---
+++ src/filefilter.c
@@ -0,0 +1,270 @@
+/* Bluefish HTML Editor
+ * filefilter.c - 
+ *
+ * Copyright (C) 2002,2003,2004,2005,2006 Olivier Sessink
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#define DEBUG
+
+
+#include <gtk/gtk.h>
+#include <string.h>
+
+#include "bluefish.h"
+#include "bf_lib.h"
+#include "dialog_utils.h"
+#include "document.h"
+#include "file.h"
+#include "filetype.h"
+#include "file_dialogs.h"
+#include "gtk_easy.h"        /* destroy_disposable_menu_cb() */
+#include "stringlist.h"      /* count_array() */
+
+
+static Tfilter *new_filter(gchar *name, gchar *mode, gchar *mimetypes) {
+	Tfilter *filter = g_new(Tfilter,1);
+	filter->name = g_strdup(name);
+	filter->mode = atoi(mode);
+	filter->filetypes = g_hash_table_new_full(g_str_hash,g_str_equal,g_free,NULL);
+	if (mimetypes){
+		gchar **types = g_strsplit(mimetypes, ":", 127);
+		gchar **type = types;
+		while (*type) {
+			g_hash_table_replace(filter->filetypes, g_strdup(*type), GINT_TO_POINTER(1));
+			type++;
+		}
+		g_strfreev(types);
+	}
+	return filter;
+}
+
+static void filter_destroy(Tfilter *filter) {
+	g_free(filter->name);
+	g_hash_table_destroy(filter->filetypes);
+	g_free(filter);
+}
+
+/* 
+ * WARNING: these filter are also used in the filechooser dialog (file->open in the menu)
+ */
+void fb2_filters_rebuild(void) {
+	GList *tmplist;
+	/* free any existing filters */
+	tmplist = g_list_first(main_v->filefilters);
+	while (tmplist) {
+		filter_destroy(tmplist->data);
+		tmplist = g_list_next(tmplist);
+	}
+	g_list_free(main_v->filefilters);
+	main_v->filefilters = NULL;
+
+	/* build a list of filters */
+	main_v->filefilters = g_list_prepend(NULL, new_filter(_("All files"), "0", NULL));
+	tmplist = g_list_first(main_v->props.filefilters);
+	while (tmplist) {
+		gchar **strarr = (gchar **) tmplist->data;
+		if (count_array(strarr) == 3) {
+			Tfilter *filter = new_filter(strarr[0], strarr[1], strarr[2]);
+			main_v->filefilters = g_list_prepend(main_v->filefilters, filter);
+		}
+		tmplist = g_list_next(tmplist);
+	}
+}
+
+/*
+the filefilter gui has one listmodel with all filetypes currently known in bluefish, 
+and two filtermodels, in_model shows all types in the filter, out_model shows all other filetypes
+and two treeviews.
+
+*/
+
+typedef struct {
+	GtkWidget *win;
+	GtkTreeModel *lmodel;
+	GtkTreeModelFilter *in_model; /* shows all the types IN the filter */
+	GtkTreeModelFilter *out_model; /* shows all types OUTside the filter */
+	GtkWidget *in_view;
+	GtkWidget *out_view;
+	
+	Tfilter *curfilter;
+} Tfilefiltergui;
+
+static void filefiltergui_destroy_lcb(GtkWidget *widget, gpointer data) {
+	Tfilefiltergui *ffg = data;
+	g_print("TODO: filefiltergui_destroy_lcb\n");
+}
+static void filefiltergui_cancel_clicked(GtkWidget *widget, gpointer data) {
+
+}
+static void filefiltergui_ok_clicked(GtkWidget *widget, gpointer data) {
+
+}
+static gboolean filefiltergui_infilter_visiblefunc(GtkTreeModel *model,GtkTreeIter *iter,gpointer data) {
+	Tfilefiltergui *ffg = data;
+	gboolean retval = FALSE;
+	gchar *mimetype=NULL;
+	gtk_tree_model_get(model, iter, 0, &mimetype, -1);
+	if (mimetype) {
+		DEBUG_MSG("filter %p, lookup %s in hashtable %p\n",ffg->curfilter,mimetype,ffg->curfilter->filetypes);
+		retval = (g_hash_table_lookup(ffg->curfilter->filetypes, mimetype) != NULL);
+		g_free(mimetype);
+	}
+	
+	return retval;
+}
+static gboolean filefiltergui_outfilter_visiblefunc(GtkTreeModel *model,GtkTreeIter *iter,gpointer data) {
+	return !filefiltergui_infilter_visiblefunc(model,iter,data);
+}
+
+static void filefiltergui_add_filetypes(gpointer key,gpointer value,gpointer data) {
+	Tfilefiltergui *ffg = data;
+	
+	if (g_hash_table_lookup(main_v->filetypetable, key) == NULL) {
+		GtkTreeIter it;
+		Tfiletype *ft = get_filetype_for_mime_type(key);
+		gtk_list_store_prepend(GTK_LIST_STORE(ffg->lmodel),&it);
+		gtk_list_store_set(GTK_LIST_STORE(ffg->lmodel),&it,0,ft->mime_type,1,ft->icon,-1);
+	}
+}
+
+static void filefiltergui_2right_clicked(GtkWidget *widget, gpointer data) {
+	Tfilefiltergui *ffg = data;
+	GtkTreeIter iter;
+	GtkTreeSelection *select;
+	GtkTreeModel *model;
+
+	/* get the selection */
+	select = gtk_tree_view_get_selection(GTK_TREE_VIEW(ffg->out_view));
+	if (gtk_tree_selection_get_selected(select, &model, &iter)) {
+		gchar *mime_type;
+		gtk_tree_model_get(model, &iter, 0, &mime_type, -1);
+		/* add the selection to the filter */
+		DEBUG_MSG("filefiltergui_2right_clicked, adding %s\n",mime_type);
+		g_hash_table_replace(ffg->curfilter->filetypes, mime_type, GINT_TO_POINTER(1));
+
+		/*g_free (mime_type);*/
+		DEBUG_MSG("filefiltergui_2right_clicked, refilter\n");
+		/* refilter */
+		gtk_tree_model_filter_refilter(ffg->in_model);
+		gtk_tree_model_filter_refilter(ffg->out_model);
+	} else {
+		DEBUG_MSG("filefiltergui_2right_clicked, nothing selected\n");
+	}
+}
+
+static void filefiltergui_2left_clicked(GtkWidget *widget, gpointer data) {
+	Tfilefiltergui *ffg = data;
+	GtkTreeIter iter;
+	GtkTreeSelection *select;
+	GtkTreeModel *model;
+
+	/* get the selection */
+	select = gtk_tree_view_get_selection(GTK_TREE_VIEW (ffg->in_view));
+	if (gtk_tree_selection_get_selected(select, &model, &iter)) {
+		gchar *mime_type;
+		gtk_tree_model_get(model, &iter, 0, &mime_type, -1);
+		/* add the selection to the filter */
+		DEBUG_MSG("filefiltergui_2left_clicked, removing %s\n",mime_type);
+		g_hash_table_remove(ffg->curfilter->filetypes, mime_type);
+
+		g_free (mime_type);
+		DEBUG_MSG("filefiltergui_2left_clicked, refilter\n");
+		/* refilter */
+		gtk_tree_model_filter_refilter(ffg->in_model);
+		gtk_tree_model_filter_refilter(ffg->out_model);
+	} else {
+		DEBUG_MSG("filefiltergui_2left_clicked, nothing selected\n");
+	}
+}
+
+void filefilter_gui(Tfilter *filter) {
+	GtkCellRenderer *renderer;
+	GtkTreeViewColumn *column;
+	GList *tmplist;
+	GtkWidget *table,*hbox,*but,*vbox;
+
+	Tfilefiltergui *ffg = g_new0(Tfilefiltergui,1);
+	ffg->curfilter = filter;
+	DEBUG_MSG("filefilter_gui, editing filter %p\n",ffg->curfilter); 
+	ffg->win = window_full2(_("Edit filter"), GTK_WIN_POS_MOUSE, 10, filefiltergui_destroy_lcb,ffg, TRUE, NULL);
+	ffg->lmodel = gtk_list_store_new(2, G_TYPE_STRING, GDK_TYPE_PIXBUF);
+	
+	/* fill the list model from the currently known filetypes */
+	tmplist = g_list_first(main_v->filetypelist);
+	while (tmplist) {
+		GtkTreeIter it;
+		Tfiletype *ft = tmplist->data;
+		if (strncmp(ft->mime_type,"x-directory",11)!=0) {
+			gtk_list_store_prepend(GTK_LIST_STORE(ffg->lmodel),&it);
+			DEBUG_MSG("filefilter_gui, adding %s\n",ft->mime_type);
+			gtk_list_store_set(GTK_LIST_STORE(ffg->lmodel),&it,0,ft->mime_type,1,ft->icon,-1);
+		}
+		tmplist = g_list_next(tmplist);
+	}
+	/* make sure that all filetypes that exist in the current filter are shown */
+	g_hash_table_foreach(ffg->curfilter->filetypes,filefiltergui_add_filetypes,ffg);
+	
+	ffg->in_model = gtk_tree_model_filter_new(ffg->lmodel, NULL);
+	gtk_tree_model_filter_set_visible_func(ffg->in_model,filefiltergui_infilter_visiblefunc,ffg,NULL);
+	ffg->out_model = gtk_tree_model_filter_new(ffg->lmodel, NULL);
+	gtk_tree_model_filter_set_visible_func(ffg->out_model,filefiltergui_outfilter_visiblefunc,ffg,NULL);
+
+	table = gtk_table_new(4,3,FALSE);
+	
+	ffg->in_view = gtk_tree_view_new_with_model(ffg->in_model);
+	renderer = gtk_cell_renderer_text_new();
+	column = gtk_tree_view_column_new_with_attributes(_("Mime type"),renderer,"text", 0,NULL);
+	gtk_tree_view_append_column (GTK_TREE_VIEW(ffg->in_view), column);
+	renderer = gtk_cell_renderer_pixbuf_new();
+	column = gtk_tree_view_column_new_with_attributes(_("Icon"),renderer,"pixbuf", 1,NULL);
+	gtk_tree_view_append_column (GTK_TREE_VIEW(ffg->in_view), column);
+	gtk_table_attach_defaults(table,ffg->in_view,2,3,1,2);
+	
+	ffg->out_view = gtk_tree_view_new_with_model(ffg->out_model);
+	renderer = gtk_cell_renderer_text_new();
+	column = gtk_tree_view_column_new_with_attributes(_("Mime type"),renderer,"text", 0,NULL);
+	gtk_tree_view_append_column (GTK_TREE_VIEW(ffg->out_view), column);
+	renderer = gtk_cell_renderer_pixbuf_new();
+	column = gtk_tree_view_column_new_with_attributes(_("Icon"),renderer,"pixbuf", 1,NULL);
+	gtk_tree_view_append_column (GTK_TREE_VIEW(ffg->out_view), column);
+	gtk_table_attach_defaults(table,ffg->out_view,0,1,1,2);
+	
+	vbox = gtk_vbox_new(TRUE,5);
+	but = gtk_button_new_with_label("->");
+	g_signal_connect(but,"clicked",filefiltergui_2right_clicked,ffg);
+	gtk_box_pack_start(vbox,but,TRUE,TRUE,0);
+	but = gtk_button_new_with_label("<-");
+	g_signal_connect(but,"clicked",filefiltergui_2left_clicked,ffg);
+	gtk_box_pack_start(vbox,but,TRUE,TRUE,0);
+	gtk_table_attach(table,vbox,1,2,1,2,GTK_EXPAND|GTK_FILL,GTK_EXPAND,5,5);
+	
+	hbox = gtk_hbutton_box_new();
+	gtk_hbutton_box_set_layout_default(GTK_BUTTONBOX_END);
+	gtk_button_box_set_spacing(GTK_BUTTON_BOX(hbox), 12);
+	but = bf_stock_cancel_button(G_CALLBACK(filefiltergui_cancel_clicked), ffg);
+	gtk_box_pack_start(GTK_BOX(hbox),but, FALSE, FALSE, 0);
+	but = bf_stock_ok_button(G_CALLBACK(filefiltergui_ok_clicked), ffg);
+	gtk_box_pack_start(GTK_BOX(hbox),but, FALSE, FALSE, 0);
+	
+	gtk_table_attach_defaults(GTK_TABLE(table), hbox, 0, 3, 3, 4);
+
+
+	gtk_container_add(ffg->win, table);
+	gtk_widget_show_all(ffg->win);
+
+	
+}
