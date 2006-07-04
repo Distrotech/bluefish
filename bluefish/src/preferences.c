9fc4b60969d0c088e2888d298dbb74c610947d44
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2006-07-04 17:51:57 +0000

    temporarily disable changes to textstyles to prevent a segfault
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@3061 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/preferences.c
+++ bluefish/src/preferences.c
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#define DEBUG
+/* #define DEBUG */
 
 #include <gtk/gtk.h>
 #include <string.h>        /* strcmp() */
@@ -1808,11 +1808,11 @@ static void preferences_apply(Tprefdialog *pd) {
 	main_v->props.textstyles = duplicate_arraylist(pd->lists[textstyles]);
 
 	/* apply the changes to highlighting patterns and filetypes to the running program */
+/*
 	textstyle_rebuild();
 
-#ifndef GNOMEVFSINT
 	bf_lang_mgr_retag();
-#endif	
+*/	
 	all_documents_apply_settings();
 	{
 		GList *tmplist = g_list_first(main_v->bfwinlist);
