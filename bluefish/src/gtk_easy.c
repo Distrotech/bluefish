f411ffbc5c186797e9eb2b1579bc5ca1d705679a
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2006-08-21 03:01:47 +0000

    Fix Bluefish Bug #337739
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/branches/bluefish_1_0@3110 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/gtk_easy.c
+++ bluefish/src/gtk_easy.c
@@ -1016,7 +1016,9 @@ gint multi_button_dialog_backend(GtkWidget *win, gchar *primary, gchar *secondar
 	hig_dialog_backend (GTK_DIALOG (dialog), primary, secondary, icon);
 	gtk_widget_show_all (dialog);
 	retval = gtk_dialog_run (GTK_DIALOG (dialog));
-	gtk_widget_destroy (dialog);
+	if (!retval == GTK_RESPONSE_DELETE_EVENT) {
+	    gtk_widget_destroy (dialog);
+	}
 
 	DEBUG_MSG("multi_button_dialog_backend: got %d\n", retval);	
 	return (retval == GTK_RESPONSE_DELETE_EVENT) ? cancelval : retval; /* When user hits escape -- give cancelval. */
