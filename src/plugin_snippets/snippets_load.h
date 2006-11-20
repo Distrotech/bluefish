44a4c0f15ca27c11e7fb3ba281a7612c3b3e7a92
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2006-11-20 22:37:42 +0000

    loading the xml file
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@3219 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/plugin_snippets/snippets_load.h
+++ src/plugin_snippets/snippets_load.h
@@ -1,5 +1,5 @@
 /* Bluefish HTML Editor
- * snippets.h - plugin for snippets sidebar
+ * snippets_load.h - plugin for snippets sidebar
  *
  * Copyright (C) 2006 Olivier Sessink
  *
@@ -17,40 +17,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-#ifndef __SNIPPETS_H_
-#define __SNIPPETS_H_
 
-#define ENABLEPLUGINS
-/* #define DEBUG */
+void snippets_load(const gchar *filename);
 
-#include <gtk/gtk.h>
-
-#include <libxml/xmlmemory.h>
-#include <libxml/parser.h>
-#include <libxml/xpath.h>
-#include <libxml/xmlwriter.h>
-
-
-#include "../config.h"
-#include "../bluefish.h"
-
-#ifdef ENABLE_NLS
-#undef _
-#define _(String) dgettext(PACKAGE"_plugin_snippets", String)
-#endif    
-
-typedef struct {
-	GHashTable* lookup; /* pointers are stored here */
-	GHashTable* data; /* xml data is stored here */
-	xmlDocPtr doc;
-	GtkTreeStore *store;
-} Tsnippets;
-
-typedef struct {
-	Tbfwin *bfwin;
-	GtkWidget *view;
-} Tsnippetswin;
-
-extern Tsnippets snippets_v;
-
-#endif /* __SNIPPETS_H_ */
