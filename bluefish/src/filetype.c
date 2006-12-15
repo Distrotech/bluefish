27f7256c78f57f693ad039d43aa1deec4070d771
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2006-12-15 18:52:40 +0000

    fixed bug http://bugzilla.gnome.org/show_bug.cgi?id=385987
    GnomeVFSFileInfo should be initialized before use
    
    
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@3315 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/filetype.c
+++ bluefish/src/filetype.c
@@ -141,15 +141,19 @@ Tfiletype *get_filetype_for_mime_type(const gchar *mime_type) {
 }
 
 const gchar *get_mimetype_for_uri(GnomeVFSURI *uri, gboolean fast) {
-	GnomeVFSFileInfo info;
+	GnomeVFSFileInfo *info;
 	GnomeVFSResult res;
+	const gchar *retval=NULL;
+	
+	info = gnome_vfs_file_info_new();
 	DEBUG_MSG("get_mimetype_for_uri, uri(%p)=%s\n",uri,gnome_vfs_uri_extract_short_name(uri));
-	res = gnome_vfs_get_file_info_uri(uri,&info,fast?GNOME_VFS_FILE_INFO_GET_MIME_TYPE|GNOME_VFS_FILE_INFO_FOLLOW_LINKS|GNOME_VFS_FILE_INFO_FORCE_FAST_MIME_TYPE:GNOME_VFS_FILE_INFO_GET_MIME_TYPE|GNOME_VFS_FILE_INFO_FOLLOW_LINKS);
+	res = gnome_vfs_get_file_info_uri(uri,info,fast?GNOME_VFS_FILE_INFO_GET_MIME_TYPE|GNOME_VFS_FILE_INFO_FOLLOW_LINKS|GNOME_VFS_FILE_INFO_FORCE_FAST_MIME_TYPE:GNOME_VFS_FILE_INFO_GET_MIME_TYPE|GNOME_VFS_FILE_INFO_FOLLOW_LINKS);
 	DEBUG_MSG("get_mimetype_for_uri, res=%d\n",res);
 	if (res == GNOME_VFS_OK) {
-		return gnome_vfs_file_info_get_mime_type(&info);
+		retval = gnome_vfs_file_info_get_mime_type(info);
 	}
-	return NULL;
+	gnome_vfs_file_info_unref(info);
+	return retval;
 }
 
 Tfiletype *get_filetype_for_uri(GnomeVFSURI *uri, gboolean fast) {
