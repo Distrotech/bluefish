6fc112d811d74ab600d3a664bca7b6ae1a84c6fa
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2007-02-04 17:11:42 +0000

    Update for changes in dialog_utils.c. Add copyright notice.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@3352 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/plugin_entities/entities.c
+++ bluefish/src/plugin_entities/entities.c
@@ -1,5 +1,27 @@
+/* Bluefish HTML Editor
+ * entities.c -
+ *
+ * Copyright (C) 2006-2007 Olivier Sessink
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
 #include <gtk/gtk.h>
 #include <string.h>
+
 #include "entities.h"
 #include "../config.h"
 #include "../plugins.h"
@@ -447,7 +469,7 @@ static void entity_dialog(Tbfwin *bfwin, Tentmode mode) {
 	gtk_combo_box_append_text(GTK_COMBO_BOX(ew->scope), _("In current document"));
 	gtk_combo_box_append_text(GTK_COMBO_BOX(ew->scope), _("In selection"));
 	gtk_combo_box_append_text(GTK_COMBO_BOX(ew->scope), _("In all documents"));
-	dialog_box_label_new(_("Sco_pe:"), 0.5, 0.5, hbox);
+	dialog_box_label_new(_("Sco_pe:"), 0.5, 0.5, hbox, 0);
 	gtk_box_pack_start(GTK_BOX(hbox), ew->scope, FALSE, FALSE, 0);
 	
 	if (mode == mode_ent2char) {
