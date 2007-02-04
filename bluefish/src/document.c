3d2817e5d7c4af575cbb1ae418bb5b08e849175e
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2007-02-04 21:44:24 +0000

    Fix Bluefish bug #394520
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/branches/bluefish_1_0@3359 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/document.c
+++ bluefish/src/document.c
@@ -1,7 +1,7 @@
 /* Bluefish HTML Editor
  * document.c - the document
  *
- * Copyright (C) 1998-2006 Olivier Sessink
+ * Copyright (C) 1998-2007 Olivier Sessink
  * Copyright (C) 1998 Chris Mazuc
  * some additions Copyright (C) 2004 Eugene Morenko(More)
  *
@@ -26,6 +26,7 @@
  * regarding the ctime_r() function
  */
 #ifdef PLATFORM_SOLARIS
+#define __EXTENSIONS__
 #define _POSIX_C_SOURCE 200312L
 #endif
 
