5b329b3d26c6ec7a65191ea52b5813492e8681cc
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2007-02-26 17:57:46 +0000

    removed some extraneous debug messages
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@3415 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/file_dialogs.c
+++ src/file_dialogs.c
@@ -1,7 +1,7 @@
 /* Bluefish HTML Editor
  * file_dialogs.c - file dialogs
  *
- * Copyright (C) 2005-2006 Olivier Sessink
+ * Copyright (C) 2005,2006,2007 Olivier Sessink
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-/* #define DEBUG */
+#define DEBUG
 
 #include <gtk/gtk.h>
 #include <string.h> /* memcpy */
