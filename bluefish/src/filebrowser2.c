7967f933e9014c7f43ae15c4606e4eb1c037e774
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2007-03-28 20:21:36 +0000

    removing the debug output, http://bugzilla.gnome.org/show_bug.cgi?id=420506 is fixed, a new bug for gnome_vfs is entered into bugzilla
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@3440 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/filebrowser2.c
+++ bluefish/src/filebrowser2.c
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#define DEBUG
+/* #define DEBUG */
 
 /* ******* FILEBROWSER DESIGN ********
 there is only one treestore left for all bluefish windows. This treestore has all files 
