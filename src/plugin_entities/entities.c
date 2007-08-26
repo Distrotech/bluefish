eb4ae8b098b851313db3ea7f73394b5cc10c0df2
dleidert <dleidert@7640d186-872a-4c71-b7b2-a5696852cc35>
2007-08-26 01:00:35 +0000

    As mentioned a long time ago, GNOME VFS offers two functions to URL-encode and -decode a string (which we already use in the mail dialog to encode the mailbody and subject. So I changed the plugin to use the GNOME VFS functions.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@3470 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/plugin_entities/entities.c
+++ src/plugin_entities/entities.c
@@ -173,61 +173,6 @@ gchar *entity_for_unichar(gunichar uchar, gboolean iso8859_1, gboolean symbols, 
 	return NULL;
 }
 
-gint gethex(gchar c) {
-	if (c >= '0' && c <= '9') return c-'0';
-	if (c >= 'a' && c <= 'f') return c-'a' + 10;
-	if (c >= 'A' && c <= 'F') return c-'A' + 10;
-	return 0;
-}
-
-gchar *urldecode(const gchar *inbuf) {
-	gchar *outbuf, *op;	/* ip = input-pointer, op=output-pointer */
-	const gchar *ip;
-
-	op = outbuf = g_malloc(strlen(inbuf)*sizeof(gchar));
-	ip = inbuf;
-	while (*ip) {
-		if (*ip == '%') {
-			if (g_ascii_isxdigit(ip[1]) && g_ascii_isxdigit(ip[2])) {
-				gchar decoded = g_ascii_xdigit_value(ip[1])*16+g_ascii_xdigit_value(ip[2]);
-				*op = decoded;
-				ip += 2;
-			} else {
-				/* invalid string */
-				g_free(outbuf);
-				return NULL;
-			}
-		} else {
-			*op = *ip;
-		}
-		op++;
-		ip++;
-	}
-	*op = '\0';
-	return outbuf;
-}
-/* for more information see http://www.ietf.org/rfc/rfc2396.txt */
-gchar *urlencode(const gchar *inbuf) {
-	gchar *outbuf, *op; /* op=output-pointer, ip=input-pointer */
-	const gchar *ip;
-	outbuf = op = g_malloc((strlen(inbuf)*3+1)*sizeof(gchar));
-	for(ip=inbuf; *ip; ip++) {
-		if ((*ip == '-' || *ip == '_' || *ip == '.' || *ip == '!' || *ip == '~' || *ip == '`') ||
-				(*ip >= '\'' && *ip <= '*') ||
-				(*ip >= '0' && *ip <= '9') ||
-				(*ip >= 'A' && *ip <= 'Z') ||
-				(*ip >= 'a' && *ip <= 'z')) {
-			*op = *ip;
-			op++;
-		} else {
-			sprintf(op, "%%%02X", *ip);
-			op += 3; 
-		}
-	}
-	*op = '\0';
-	return outbuf;
-}
-
 typedef enum {
 	mode_urlencode,
 	mode_urldecode,
@@ -243,10 +188,10 @@ static void doc_code_selection(Tdocument *doc, Treplace_mode mode) {
 		inbuf = doc_get_chars(doc,start,end);
 		switch (mode) {
 			case mode_urlencode:
-				outbuf = urlencode(inbuf);
+				outbuf = gnome_vfs_escape_string(inbuf);
 			break;
 			case mode_urldecode:
-				outbuf = urldecode(inbuf);
+				outbuf = gnome_vfs_unescape_string(inbuf, NULL);
 			break;
 			case mode_tolowercase:
 				if (inbuf) outbuf = g_utf8_strdown(inbuf,-1);
