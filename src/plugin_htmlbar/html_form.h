b91f3e7e3578c87a44d2f8a62c7c9fd586ff4040
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2007-08-27 16:23:11 +0000

    another tiny commit to test the list
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@3475 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/plugin_htmlbar/html_form.h
+++ src/plugin_htmlbar/html_form.h
@@ -1,7 +1,7 @@
 /* Bluefish HTML Editor
  * html_form.h - menu/toolbar callback prototypes
  *
- * Copyright (C) 2000 Olivier Sessink
+ * Copyright (C) 2000-2007 Olivier Sessink
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
