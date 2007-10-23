da078759392237c856621998c8e4fa954bd327e5
dleidert <dleidert@7640d186-872a-4c71-b7b2-a5696852cc35>
2007-10-23 14:33:38 +0000

    Remove error code.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@3565 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/bluefish.c
+++ bluefish/src/bluefish.c
@@ -158,8 +158,8 @@ int main(int argc, char *argv[])
 	g_option_context_set_ignore_unknown_options (context, TRUE);
 	g_option_context_add_group (context, gtk_get_option_group (TRUE));
 	if (!g_option_context_parse (context, &argc, &argv, &error)) {
-		g_error(N_("Error code %d parsing command line options. %s\nPlease run: %s -?\n"),
-		        error->code, error->message, argv[0]);
+		g_error(N_("Error parsing command line options. %s\nPlease run: %s -?\n"),
+		        error->message, argv[0]);
 	}
 
 	g_option_context_free (context);
