efd8aed4b38c0aaf97551841f4e7b0f7cd4976a2
dleidert <dleidert@7640d186-872a-4c71-b7b2-a5696852cc35>
2007-10-23 02:09:55 +0000

    Ignore unknown options and handle errors parsing the commandline options.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@3560 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/bluefish.c
+++ bluefish/src/bluefish.c
@@ -154,8 +154,15 @@ int main(int argc, char *argv[])
 #endif /* GNOME_PARAM_GOPTION_CONTEXT */
 	                                NULL);
 #else /* HAVE_LIBGNOMEUI_LIBGNOMEUI_H */
+	/* this one or bail out with the error of g_option_context_parse () */
+	g_option_context_set_ignore_unknown_options (context, TRUE);
 	g_option_context_add_group (context, gtk_get_option_group (TRUE));
-	g_option_context_parse (context, &argc, &argv, &error);
+	if (!g_option_context_parse (context, &argc, &argv, &error)) {
+		g_error(N_("%s\nPlease run: %s -?\n"), error->message, argv[0]);
+		g_error_free (error);
+		g_free (argv);
+		exit (-1);
+	}
 #endif /* HAVE_LIBGNOMEUI_LIBGNOMEUI_H */
 
 	xmlInitParser();
