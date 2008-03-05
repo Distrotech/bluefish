514352e33694c2d97fa491661b99d7bca1c534f8
dleidert <dleidert@7640d186-872a-4c71-b7b2-a5696852cc35>
2008-03-05 18:15:26 +0000

    Typo fix. http://bugzilla.gnome.org/show_bug.cgi?id=520537
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@3619 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/plugin_htmlbar/htmlbar_gui.c
+++ src/plugin_htmlbar/htmlbar_gui.c
@@ -331,7 +331,7 @@ void htmlbar_build_menu(Thtmlbarwin *hbw) {
 		{N_("/Tags/Special/Other/_Masculine ordinal º"), NULL, insert_char_cb, 92, "<Item>"},
 		{N_("/Tags/Special/Other/_Pilcrow (paragraph sign) ¶"), NULL, insert_char_cb, 93, "<Item>"},
 		{N_("/Tags/Special/Other/_Broken bar ¦"), NULL, insert_char_cb, 94, "<Item>"},
-		{N_("/Tags/Special/Other/Soft _hypen -"), NULL, insert_char_cb, 95, "<Item>"},
+		{N_("/Tags/Special/Other/Soft _hyphen -"), NULL, insert_char_cb, 95, "<Item>"},
 		{N_("/Tags/Special/Other/Spa_cing macron ¯"), NULL, insert_char_cb, 96, "<Item>"},
 		{N_("/Tags/Special/Other/Middle _dot ·"), NULL, insert_char_cb, 97, "<Item>"},
 		{N_("/Tags/_Format by layout"), NULL, NULL, 0, "<Branch>"},
