38728e028e7a68000892e69e1aeebd50741afeeb
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2008-08-31 19:17:14 +0000

    adding the test widget to cvs
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@3770 7640d186-872a-4c71-b7b2-a5696852cc35

---
+++ testcode/bftestview2/bftextview2.c
@@ -0,0 +1,92 @@
+#include "bftextview2.h"
+#include "bftextview2_scanner.h"
+
+static void bftextview2_scanner_idle(Tbftextview2 * bt2) {
+	if (!bftextview2_run_scanner(bt2)) {
+		bt2->scanner_idle = 0;
+		return FALSE;
+	}
+	return TRUE;
+}
+
+static void bftextview2_insert_text_lcb(GtkTextBuffer * buffer, GtkTextIter * iter,
+										gchar * string, gint stringlen, Tbftextview2 * bt2)
+{
+	GtkTextIter start;
+	g_print("bftextview2_insert_text_lcb, stringlen=%d\n",stringlen);
+	if (bt2->scanner_idle == 0) {
+		bt2->scanner_idle = g_idle_add(bftextview2_run_scanner,bt2);
+	}
+	
+	/* mark the text that is changed */
+	start = *iter;
+	gtk_text_iter_backward_chars(&start,stringlen);
+	/* always scan the whole line */
+	gtk_text_iter_set_line_offset(&start, 0);
+	gtk_text_iter_forward_to_line_end(iter);
+	gtk_text_buffer_apply_tag_by_name(buffer,"needscanning",&start,iter);
+	g_print("mark text from %d to %d as needscanning\n",gtk_text_iter_get_offset(&start),gtk_text_iter_get_offset(iter));
+	
+}
+
+/* widget stuff below */
+
+
+static GtkTextViewClass *parent_class = NULL;
+
+static void bftextview2_class_init(Tbftextview2Class * c)
+{
+	parent_class = g_type_class_ref(GTK_TYPE_TEXT_VIEW);
+}
+
+static void bftextview2_init(Tbftextview2 * bt2)
+{
+	bt2->timer = g_timer_new();
+
+}
+
+GType bftextview2_get_type(void)
+{
+	static GType type = 0;
+
+	if (type == 0) {
+		static const GTypeInfo info = {
+			sizeof(Tbftextview2Class),
+			(GBaseInitFunc) NULL,
+			(GBaseFinalizeFunc) NULL,
+			(GClassInitFunc) bftextview2_class_init,
+			(GClassFinalizeFunc) NULL,
+			NULL /* class_data */ ,
+			sizeof(Tbftextview2),
+			0 /* n_preallocs */ ,
+			(GInstanceInitFunc) bftextview2_init,
+			NULL
+		};
+		type = g_type_register_static(GTK_TYPE_TEXT_VIEW, "bftextview2", &info, (GTypeFlags) 0);
+	}
+	return type;
+}
+
+GtkWidget *bftextview2_new(void)
+{
+	PangoFontDescription *font_desc;
+	Tbftextview2 *o = ((Tbftextview2 *) g_object_new(bftextview2_get_type(), NULL));
+	
+	/*g_signal_connect(G_OBJECT(o), "expose-event", G_CALLBACK(bftextview2_expose_lcb), NULL);*/
+	g_signal_connect_after(G_OBJECT(gtk_text_view_get_buffer(GTK_TEXT_VIEW(o))), "insert-text",
+						   G_CALLBACK(bftextview2_insert_text_lcb), o);
+	font_desc = pango_font_description_from_string("Courier 11");
+	gtk_widget_modify_font(o, font_desc);
+	pango_font_description_free(font_desc);
+
+	return (GtkWidget *) o;
+}
+
+GtkWidget *bftextview2_new_with_buffer(GtkTextBuffer * buffer)
+{
+	Tbftextview2 *o = bftextview2_new();
+	gtk_text_view_set_buffer(GTK_TEXT_VIEW(o), buffer);
+	g_signal_connect_after(G_OBJECT(gtk_text_view_get_buffer(GTK_TEXT_VIEW(o))), "insert-text",
+						   G_CALLBACK(bftextview2_insert_text_lcb), o);
+	return (GtkWidget *) o;
+}
---
+++ testcode/bftestview2/bftextview2.h
@@ -0,0 +1,101 @@
+#ifndef _BFTEXTVIEW_H_
+#define _BFTEXTVIEW_H_
+
+#include <gtk/gtk.h>
+
+#define NUMSCANCHARS 127
+#define MAXSCANSTATES 1000
+#define MAXMATCHES 10
+#define NUMCONTEXTS 10
+
+/* building the automata */
+
+typedef struct {
+	GCompletion* ac;
+
+} Tcontext;
+
+typedef struct {
+	char *message;
+	int nextcontext;
+	GtkTextTag *selftag;
+	GtkTextTag *blocktag;
+} Tpattern;
+
+typedef struct {
+	unsigned int row[NUMSCANCHARS];	/* contains for each character the number of the next state */
+	unsigned int match;			/* 0 == no match, refers to the index number in array 'matches' */
+} Ttablerow;
+
+typedef struct {
+	int nextnewpos;
+	int nextnewmatch;
+	GArray *table;
+	GArray *matches;
+	GArray *contexts;
+} Tscantable;
+
+
+/* scanning the text */
+typedef struct {
+	GtkTextMark *start1;
+	GtkTextMark *end1;
+	GtkTextMark *start2;
+	GtkTextMark *end2;
+} Tfoundblock; /* once a start-of-block is found start1 and end1 are set 
+						and the Tfoundblock is added to the GtkTextMark's as "block"
+						and the Tfoundblock is added to the current blockstack.
+						A copy of the current blockstack is copied into Tscancache
+						so we can later on find what the current blockstack looks like.
+						once the end-of-block is found, start2 and end2 are set 
+						and the Tfoundblock is added to these GtkTextMark's as "block"
+						The Tfoundblock is popped from the current stack, and a new copy
+						of the stack is copied into Tscancache */
+
+typedef struct {
+	GtkTextMark *start;
+	GtkTextMark *end;
+	gint contextnum;
+} Tfoundcontext; /* once a start-of-context is found start is set 
+						and the Tfoundcontext is added to the GtkTextMark as "context"
+						and the Tfoundcontext is added to the current contextstack.
+						A copy of the current contextstack is copied into Tscancache
+						so we can later on find what the current contextstack looks like.
+						once the end-of-context is found, end is set 
+						and the Tfoundcontext is added to this GtkTextMark as "context"
+						The Tfoundcontext is popped from the current stack, and a new copy
+						of the stack is copied into Tscancache */ 
+
+typedef enum {
+	CONTEXTSTACK,
+	BLOCKSTACK
+} Tstacktype;
+
+typedef struct {
+	GQueue *stack; /* a Tfoundcontext or Tfoundblock stack */
+	Tstacktype type;
+} Tfoundstack;
+
+typedef struct {
+	GSequence* stackcaches; /* a sorted cache of all blocks and context stacks for 
+							each position where the stack changes so we can restart scanning 
+							on any location */
+} Tscancache;
+
+/* stuff for the widget */
+
+typedef struct {
+	GtkTextView __parent__;
+	Tscantable *scantable;
+	Tscancache scancache;
+	guint scanner_idle; /* 0 if no idle function is running */
+	GTimer *timer;
+} Tbftextview2;
+
+typedef struct {
+	GtkTextViewClass __parent__;
+} Tbftextview2Class;
+
+extern Tscantable scantable;
+
+#endif
