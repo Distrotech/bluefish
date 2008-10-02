bb8145101228834dd84128fe4d94f751793df36c
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2008-10-02 20:50:20 +0000

    added html patterns
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@3906 7640d186-872a-4c71-b7b2-a5696852cc35

--- testcode/bftestview2/bftextview2_langmgr.c
+++ testcode/bftestview2/bftextview2_langmgr.c
@@ -367,5 +367,8 @@ void langmgr_init(void) {
 	register_bflanguage(bflang);
 	bflang = parse_bflang2_header("php.bflang2");
 	register_bflanguage(bflang);
+	bflang = parse_bflang2_header("html.bflang2");
+	register_bflanguage(bflang);
+
 	DBG_PARSING("langmgr_init, returning \n");
 }
