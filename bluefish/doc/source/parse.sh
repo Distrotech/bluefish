fbc1a52f62d106f1fb13797c8bc96cdc17f0abb9
dleidert <dleidert@7640d186-872a-4c71-b7b2-a5696852cc35>
2008-11-01 17:43:33 +0000

    clean-up: Remove /bluefish/ from tags/branches.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/tags/release_0_12@4014 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/doc/source/parse.sh
+++
@@ -1,1 +0,0 @@
-nsgmls -sv -c ../../../bluefish-doctools/tools/dtd/docbook41/docbook.cat ../../../bluefish-doctools/tools/dtd/xml.dcl book.xml
--- bluefish/src/rpopup.h
+++
@@ -1,2 +0,0 @@
-gboolean doc_bevent_in_html_tag(Tdocument *doc, GdkEventButton *bevent);
-void rpopup_edit_tag_cb(GtkMenuItem *menuitem,Tdocument *doc);
