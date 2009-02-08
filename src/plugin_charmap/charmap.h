b8bf02158a0b3e1dbfa17d41577fff60e4ede09c
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2009-02-08 15:51:58 +0000

    Add missing GPL headers to the charmap plugin
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@4548 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/plugin_charmap/charmap.h
+++ src/plugin_charmap/charmap.h
@@ -1,3 +1,31 @@
+/* Bluefish HTML Editor
+ * charmap.h - charmap plugin
+ *
+ * Copyright (C) 2009 Olivier Sessink
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __CHARMAP_H_
+#define __CHARMAP_H_
+
+#ifdef ENABLE_NLS
+#undef _
+#define _(String) dgettext(PACKAGE"_plugin_about", String)
+#endif
+
 #include <gmodule.h>
 #include <gdk/gdk.h>
 #include <gtk/gtk.h>
@@ -15,3 +43,6 @@ typedef struct {
 	GtkWidget *chaptersv;
 	GtkWidget *gcm;
 }Tcharmap;
+
+
+#endif /* __CHARMAP_H_  */
