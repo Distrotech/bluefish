abd05e12fa455e59e93eef3c5e8ab6a9813ec360
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2009-02-08 15:51:58 +0000

    Add missing GPL headers to the charmap plugin
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@4548 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/plugin_charmap/charmap_gui.h
+++ bluefish/src/plugin_charmap/charmap_gui.h
@@ -1,2 +1,27 @@
+/* Bluefish HTML Editor
+ * charmap_gui.h - charmap plugin
+ *
+ * Copyright (C) 2009 Olivier Sessink
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __CHARMAP_GUI_H_
+#define __CHARMAP_GUI_H_
+
 void charmap_sidepanel_destroygui(Tbfwin *bfwin);
 void charmap_sidepanel_initgui(Tbfwin *bfwin);
+
+#endif /* __CHARMAP_GUI_H_  */
