529bf39259752a0a7338ff44d503eb958d7b5c7d
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2009-02-12 13:59:30 +0000

    charmap plugin is now also functional with gucharmap-2
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@4560 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/plugin_charmap/charmap_gui.c
+++ bluefish/src/plugin_charmap/charmap_gui.c
@@ -27,10 +27,11 @@ void charmap_sidepanel_destroygui(Tbfwin *bfwin) {
 	
 }
 #ifdef HAVE_LIBGUCHARMAP_2
-static void charmap_charmap_activate_lcb(GtkWidget *chartable,gunichar wc,gpointer data) {
+static void charmap_charmap_activate_lcb(GucharmapChartable *gucharmapchartable,gpointer data) {
 	Tcharmap *cm = data;
 	gchar ubuf[7];
-
+	gunichar wc;
+	wc = gucharmap_chartable_get_active_character(gucharmapchartable);
 	ubuf[g_unichar_to_utf8 (wc, ubuf)] = '\0';
 	g_print("charmap_plugin, clicked %s\n",ubuf);
 	doc_insert_two_strings(cm->bfwin->current_document, ubuf, NULL);
@@ -59,7 +60,11 @@ static void chaptersv_changed_lcb(GtkComboBox *combo, gpointer data) {
 		GucharmapCodepointList * gcpl;
 		gcpl = gucharmap_script_codepoint_list_new();
 		gucharmap_script_codepoint_list_set_script(gcpl,name);
+#ifdef HAVE_LIBGUCHARMAP_1		
 		gucharmap_table_set_codepoint_list(cm->gcm,gcpl);
+#else
+		gucharmap_chartable_set_codepoint_list (cm->gcm,gcpl);
+#endif 
 		g_free(name);
 	}
 }
@@ -89,12 +94,15 @@ void charmap_sidepanel_initgui(Tbfwin *bfwin) {
 	g_signal_connect(G_OBJECT(cm->chaptersv), "changed",G_CALLBACK(chaptersv_changed_lcb),cm);
 	g_object_unref(model); 
 	gtk_box_pack_start(GTK_BOX(vbox),cm->chaptersv,FALSE,TRUE,2);
-	
-	
+#ifdef HAVE_LIBGUCHARMAP_1
 	cm->gcm = gucharmap_table_new();
+#else
+	cm->gcm = gucharmap_chartable_new();
+#endif 
 	gcpl = gucharmap_script_codepoint_list_new();
+/*
 	gucharmap_script_codepoint_list_set_script(gcpl,"Nko");
-	gucharmap_table_set_codepoint_list(cm->gcm,gcpl);
+	gucharmap_table_set_codepoint_list(cm->gcm,gcpl);*/
 	g_signal_connect(cm->gcm, "activate", G_CALLBACK(charmap_charmap_activate_lcb), cm);
 	gtk_combo_box_set_active(cm->chaptersv,0);
 	/* these lines create a full gucharmap with vertical pane inside the sidebar
@@ -106,8 +114,11 @@ void charmap_sidepanel_initgui(Tbfwin *bfwin) {
 	
 	scrolwin = gtk_scrolled_window_new(NULL, NULL);
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolwin), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+#ifdef HAVE_LIBGUCHARMAP_1
 	gtk_scrolled_window_add_with_viewport(scrolwin, cm->gcm);
-	/*gtk_container_add(GTK_CONTAINER(scrolwin), cm->gcm);*/
+#else
+	gtk_container_add(GTK_CONTAINER(scrolwin), cm->gcm);
+#endif
 	gtk_box_pack_start(GTK_BOX(vbox),scrolwin,TRUE,TRUE,4);
 	
 	gtk_notebook_insert_page_menu(GTK_NOTEBOOK(bfwin->leftpanel_notebook),vbox,gtk_label_new(_("charmap")),gtk_label_new(_("charmap")),2);
