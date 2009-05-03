fa70151008c59cfff9f0e9c65366c542879cc42a
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2009-05-03 09:12:37 +0000

    adding the option to the preferences dialog, but not yet in preferences_apply()
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@4797 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/bftextview2_langmgr.h
+++ bluefish/src/bftextview2_langmgr.h
@@ -28,6 +28,7 @@ void langmgr_reload_user_highlights(void);
 GtkTextTag *langmrg_lookup_tag_highlight(const gchar *lang, const gchar *highlight);
 GtkTextTagTable *langmgr_get_tagtable(void);
 Tbflang *langmgr_get_bflang_for_mimetype(const gchar *mimetype);
+GList *langmgr_get_languages_mimetypes(void);
 GList *langmgr_get_languages(void);
 void langmgr_init(void);
 void langmgr_cleanup(void);
