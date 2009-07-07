5e8e2a912fb44426f938f5d27132046444b20603
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2009-07-07 20:38:55 +0000

    add spell header
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@4912 7640d186-872a-4c71-b7b2-a5696852cc35

---
+++ bluefish/src/bftextview2_spell.h
@@ -0,0 +1,32 @@
+/* Bluefish HTML Editor
+ * bftextview2_spell.h
+ *
+ * Copyright (C) 2009 Olivier Sessink
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+/* for the design docs see bftextview2.h */
+#ifndef _BFTEXTVIEW2_SPELL_H_
+#define _BFTEXTVIEW2_SPELL_H_
+
+#include "bftextview2.h"
+
+gboolean bftextview2_run_spellcheck(BluefishTextView * btv);
+void bftextview2_spell_init(void);
+void bftextview2_spell_cleanup(void);
+void bftextview2_populate_suggestions_popup(GtkMenu *menu, Tdocument *doc);
+void bftextview2_populate_preferences_popup(GtkMenu *menu, Tdocument *doc);
+#endif
