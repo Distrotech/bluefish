71487d1f9e0e971eb4906b5d74b7d3e3ff27b220
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2009-07-16 11:43:42 +0000

    work on http://bugzilla.gnome.org/show_bug.cgi?id=585952
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@4929 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/plugin_snippets/snippets_gui.c
+++ src/plugin_snippets/snippets_gui.c
@@ -1,7 +1,7 @@
 /* Bluefish HTML Editor
  * snippets_gui.c - plugin for snippets sidebar
  *
- * Copyright (C) 2006-2007 Olivier Sessink
+ * Copyright (C) 2006-2009 Olivier Sessink
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
