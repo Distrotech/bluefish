a8343d0b628e8c0f36a0e4950d96ffe662c1c1f4
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2010-08-16 20:49:01 +0000

    move to GPL 3
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@6052 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/bftextview2_scanner.c
+++ bluefish/src/bftextview2_scanner.c
@@ -5,7 +5,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -20,7 +20,7 @@
 
 /*#define MINIMAL_REFCOUNTING*/
 
-/*#define HL_PROFILING*/
+#define HL_PROFILING
 #ifdef HL_PROFILING
 #include <unistd.h>
 #endif
