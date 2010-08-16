1bb1fc5e6c8f4eaae5f13237f3f9a6fbd7b093d6
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2010-08-16 20:49:01 +0000

    move to GPL 3
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@6052 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/plugin_about/about.c
+++ src/plugin_about/about.c
@@ -6,7 +6,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -148,7 +148,7 @@ static void about_dialog_create(Tbfwin *bfwin, guint *callback_action, GtkWidget
   const gchar *license =
     "This program is free software; you can redistribute it and/or modify it\n"
     "under the terms of the GNU General Public License as published by\n"
-    "the Free Software Foundation; either version 2 of the License, or\n"
+    "the Free Software Foundation; either version 3 of the License, or\n"
     "(at your option) any later version.\n"
     "\n"
     "This program is distributed in the hope that it will be useful,\n"
