b8b8543f47f059fcb786388d58d8f7e9dacefbc5
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2011-03-13 01:50:30 +0000

    Replace the deprecated GtkSignalFunc with GCallback.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/branches/bluefish_gtk3@6514 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/plugin_htmlbar/html_diag.c
+++ src/plugin_htmlbar/html_diag.c
@@ -1,7 +1,7 @@
 /* Bluefish HTML Editor
  * html_diag.c - general functions to create HTML dialogs
  *
- * Copyright (C) 2000-2010 Olivier Sessink
+ * Copyright (C) 2000-2011 Olivier Sessink
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,19 +17,20 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <gtk/gtk.h>
-#include <string.h>	/* strrchr */
-#include <stdlib.h> /* strtod */
-/*#define DEBUG */
+/*#define DEBUG*/
 
-#include "htmlbar.h"
-#include "../gtk_easy.h" /* window_full() */
-#include "../bf_lib.h"
-#include "../stringlist.h" /* add_to_stringlist */
-#include "../document.h" /* doc_save_selection */
+#include <gtk/gtk.h>
+#include <string.h>				/* strrchr */
+#include <stdlib.h>				/* strtod */
+
+#include "html_diag.h"			/* myself */
 #include "cap.h"
-#include "html2.h" /* style_but_new() */
-#include "html_diag.h" /* myself */
+#include "html2.h"				/* style_but_new() */
+#include "htmlbar.h"
+#include "../gtk_easy.h"		/* window_full() */
+#include "../bf_lib.h"
+#include "../stringlist.h"		/* add_to_stringlist */
+#include "../document.h"		/* doc_save_selection */
 
 
 Trecent_attribs recent_attribs;
@@ -37,35 +38,42 @@ Trecent_attribs recent_attribs;
 /********** DIALOG FUNCTIONS *************/
 /*****************************************/
 
-void html_diag_destroy_cb(GtkWidget * widget, Thtml_diag *dg) {
+void
+html_diag_destroy_cb(GtkWidget * widget, Thtml_diag * dg)
+{
 	dg->tobedestroyed = TRUE;
-	DEBUG_MSG("html_diag_destroy_cb, widget=%p, dg=%p, dg->dialog=%p\n",widget,dg,dg->dialog);
-	if (gtk_text_buffer_get_mark(dg->doc->buffer,"diag_ins") == dg->mark_ins) { 
-		gtk_text_buffer_delete_mark(dg->doc->buffer,dg->mark_ins);
-		gtk_text_buffer_delete_mark(dg->doc->buffer,dg->mark_sel);
+	DEBUG_MSG("html_diag_destroy_cb, widget=%p, dg=%p, dg->dialog=%p\n", widget, dg, dg->dialog);
+	if (gtk_text_buffer_get_mark(dg->doc->buffer, "diag_ins") == dg->mark_ins) {
+		gtk_text_buffer_delete_mark(dg->doc->buffer, dg->mark_ins);
+		gtk_text_buffer_delete_mark(dg->doc->buffer, dg->mark_sel);
 	}
 	window_destroy(dg->dialog);
-	DEBUG_MSG("html_diag_destroy_cb, about to free dg=%p\n",dg);
+	DEBUG_MSG("html_diag_destroy_cb, about to free dg=%p\n", dg);
 	g_free(dg);
 }
 
-void html_diag_cancel_clicked_cb(GtkWidget *widget, gpointer data) {
+void
+html_diag_cancel_clicked_cb(GtkWidget * widget, gpointer data)
+{
 	html_diag_destroy_cb(NULL, data);
 }
 
-Thtml_diag *html_diag_new(Tbfwin *bfwin, gchar *title) {
+Thtml_diag *
+html_diag_new(Tbfwin * bfwin, gchar * title)
+{
 	Thtml_diag *dg;
-	
+
 	if (!bfwin) {
 		g_warning("plugin_htmlbar: bfwin may not be NULL in html_diag_new()\n");
 		return NULL;
 	}
-	
+
 	dg = g_malloc(sizeof(Thtml_diag));
 	dg->tobedestroyed = FALSE;
-	DEBUG_MSG("html_diag_new, dg=%p\n",dg);
-	dg->dialog = window_full2(title, GTK_WIN_POS_MOUSE
-		, 12,G_CALLBACK(html_diag_destroy_cb), dg, TRUE,  bfwin->main_window);
+	DEBUG_MSG("html_diag_new, dg=%p\n", dg);
+	dg->dialog =
+		window_full2(title, GTK_WIN_POS_MOUSE, 12, G_CALLBACK(html_diag_destroy_cb), dg, TRUE,
+					 bfwin->main_window);
 	gtk_window_set_type_hint(GTK_WINDOW(dg->dialog), GDK_WINDOW_TYPE_HINT_DIALOG);
 	gtk_window_set_role(GTK_WINDOW(dg->dialog), "html_dialog");
 	dg->vbox = gtk_vbox_new(FALSE, 1);
@@ -74,25 +82,26 @@ Thtml_diag *html_diag_new(Tbfwin *bfwin, gchar *title) {
 	{
 		Tdocument *doc = bfwin->current_document;
 		GtkTextIter iter1, iter2;
-		GtkTextMark* mark;
-		mark = gtk_text_buffer_get_mark(doc->buffer,"insert");
-		gtk_text_buffer_get_iter_at_mark(doc->buffer,&iter1,mark);
-		mark = gtk_text_buffer_get_mark(doc->buffer,"selection_bound");
-		gtk_text_buffer_get_iter_at_mark(doc->buffer,&iter2,mark);
-		g_print("html_diag_new, current marks: insert=%d, selection=%d\n",gtk_text_iter_get_offset(&iter1),gtk_text_iter_get_offset(&iter2));
+		GtkTextMark *mark;
+		mark = gtk_text_buffer_get_mark(doc->buffer, "insert");
+		gtk_text_buffer_get_iter_at_mark(doc->buffer, &iter1, mark);
+		mark = gtk_text_buffer_get_mark(doc->buffer, "selection_bound");
+		gtk_text_buffer_get_iter_at_mark(doc->buffer, &iter2, mark);
+		g_print("html_diag_new, current marks: insert=%d, selection=%d\n", gtk_text_iter_get_offset(&iter1),
+				gtk_text_iter_get_offset(&iter2));
 	}
 #endif
 
-	if (!gtk_text_buffer_get_mark(bfwin->current_document->buffer,"diag_ins")) {
+	if (!gtk_text_buffer_get_mark(bfwin->current_document->buffer, "diag_ins")) {
 		GtkTextIter iter;
-		GtkTextMark* mark = gtk_text_buffer_get_mark(bfwin->current_document->buffer,"insert");
-		gtk_text_buffer_get_iter_at_mark(bfwin->current_document->buffer,&iter,mark);
-		dg->mark_ins = gtk_text_buffer_create_mark(bfwin->current_document->buffer,"diag_ins",&iter,TRUE);
-		DEBUG_MSG("html_diag_new, setting diag_ins at %d",gtk_text_iter_get_offset(&iter));
-		mark = gtk_text_buffer_get_mark(bfwin->current_document->buffer,"selection_bound");
-		gtk_text_buffer_get_iter_at_mark(bfwin->current_document->buffer,&iter,mark);
-		dg->mark_sel = gtk_text_buffer_create_mark(bfwin->current_document->buffer,"diag_sel",&iter,TRUE);
-		DEBUG_MSG(" and diag_sel at %d\n",gtk_text_iter_get_offset(&iter));
+		GtkTextMark *mark = gtk_text_buffer_get_mark(bfwin->current_document->buffer, "insert");
+		gtk_text_buffer_get_iter_at_mark(bfwin->current_document->buffer, &iter, mark);
+		dg->mark_ins = gtk_text_buffer_create_mark(bfwin->current_document->buffer, "diag_ins", &iter, TRUE);
+		DEBUG_MSG("html_diag_new, setting diag_ins at %d", gtk_text_iter_get_offset(&iter));
+		mark = gtk_text_buffer_get_mark(bfwin->current_document->buffer, "selection_bound");
+		gtk_text_buffer_get_iter_at_mark(bfwin->current_document->buffer, &iter, mark);
+		dg->mark_sel = gtk_text_buffer_create_mark(bfwin->current_document->buffer, "diag_sel", &iter, TRUE);
+		DEBUG_MSG(" and diag_sel at %d\n", gtk_text_iter_get_offset(&iter));
 	} else {
 		DEBUG_MSG("html_diag_new, another dialog is open, marking mark_ins and mark_sel = NULL\n");
 		dg->mark_ins = dg->mark_sel = NULL;
@@ -112,7 +121,9 @@ Thtml_diag *html_diag_new(Tbfwin *bfwin, gchar *title) {
 	return dg;
 }
 
-GtkWidget *html_diag_table_in_vbox(Thtml_diag *dg, gint rows, gint cols) {
+GtkWidget *
+html_diag_table_in_vbox(Thtml_diag * dg, gint rows, gint cols)
+{
 	GtkWidget *returnwidget = gtk_table_new(rows, cols, FALSE);
 	gtk_table_set_row_spacings(GTK_TABLE(returnwidget), 12);
 	gtk_table_set_col_spacings(GTK_TABLE(returnwidget), 12);
@@ -120,19 +131,21 @@ GtkWidget *html_diag_table_in_vbox(Thtml_diag *dg, gint rows, gint cols) {
 	return returnwidget;
 }
 
-void html_diag_finish(Thtml_diag *dg, GtkSignalFunc ok_func) {
+void
+html_diag_finish(Thtml_diag * dg, GCallback ok_func)
+{
 	GtkWidget *hbox;
 
 	gtk_box_pack_start(GTK_BOX(dg->vbox), gtk_hseparator_new(), FALSE, FALSE, 12);
 	hbox = gtk_hbutton_box_new();
 	gtk_hbutton_box_set_layout_default(GTK_BUTTONBOX_END);
 	gtk_button_box_set_spacing(GTK_BUTTON_BOX(hbox), 12);
-	
+
 	dg->obut = bf_stock_ok_button(ok_func, dg);
 	dg->cbut = bf_stock_cancel_button(G_CALLBACK(html_diag_cancel_clicked_cb), dg);
 
-	gtk_box_pack_start(GTK_BOX(hbox),dg->cbut , FALSE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(hbox),dg->obut , FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(hbox), dg->cbut, FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(hbox), dg->obut, FALSE, FALSE, 0);
 	gtk_window_set_default(GTK_WINDOW(dg->dialog), dg->obut);
 
 	gtk_box_pack_start(GTK_BOX(dg->vbox), hbox, FALSE, FALSE, 0);
@@ -143,9 +156,10 @@ void html_diag_finish(Thtml_diag *dg, GtkSignalFunc ok_func) {
 /********** HTML -> DIALOG FUNCTIONS *************/
 /*************************************************/
 
-void parse_html_for_dialogvalues(gchar *dialogitems[], gchar *dialogvalues[]
-		, gchar **custom, Ttagpopup *data) {
-	gint count=0;
+void
+parse_html_for_dialogvalues(gchar * dialogitems[], gchar * dialogvalues[], gchar ** custom, Ttagpopup * data)
+{
+	gint count = 0;
 	gchar *customnew;
 	GList *tmplist;
 	gboolean found = FALSE;
@@ -158,7 +172,8 @@ void parse_html_for_dialogvalues(gchar *dialogitems[], gchar *dialogvalues[]
 		found = FALSE;
 		while (dialogitems[count]) {
 			if (strcmp(((Ttagitem *) tmplist->data)->item, dialogitems[count]) == 0) {
-				DEBUG_MSG("fill_dialogvalues, string found!, dialogitems[%d]=%s\n", count, dialogitems[count]);
+				DEBUG_MSG("fill_dialogvalues, string found!, dialogitems[%d]=%s\n", count,
+						  dialogitems[count]);
 				dialogvalues[count] = ((Ttagitem *) tmplist->data)->value;
 				DEBUG_MSG("fill_dialogvalues, *dialogvalues[%d]=%s\n", count, dialogvalues[count]);
 				found = TRUE;
@@ -167,11 +182,13 @@ void parse_html_for_dialogvalues(gchar *dialogitems[], gchar *dialogvalues[]
 		}
 		if (!found) {
 			customnew = g_strjoin(NULL, *custom, " ", ((Ttagitem *) tmplist->data)->item, NULL);
-			if (*custom) g_free(*custom);
+			if (*custom)
+				g_free(*custom);
 			*custom = customnew;
 			if (((Ttagitem *) tmplist->data)->value) {
 				customnew = g_strjoin(NULL, *custom, "=\"", ((Ttagitem *) tmplist->data)->value, "\"", NULL);
-				if (*custom) g_free(*custom);
+				if (*custom)
+					g_free(*custom);
 				*custom = customnew;
 			}
 		}
@@ -179,11 +196,13 @@ void parse_html_for_dialogvalues(gchar *dialogitems[], gchar *dialogvalues[]
 	}
 }
 
-void fill_dialogvalues(gchar *dialogitems[], gchar *dialogvalues[]
-		, gchar **custom, Ttagpopup *data, Thtml_diag *diag) {
+void
+fill_dialogvalues(gchar * dialogitems[], gchar * dialogvalues[],
+				  gchar ** custom, Ttagpopup * data, Thtml_diag * diag)
+{
 
-	gint count=0;
-	
+	gint count = 0;
+
 	while (dialogitems[count]) {
 		dialogvalues[count] = NULL;
 		count++;
@@ -204,7 +223,8 @@ void fill_dialogvalues(gchar *dialogitems[], gchar *dialogvalues[]
 	}
 }
 
-void parse_existence_for_dialog(gchar * valuestring, GtkWidget * checkbox)
+void
+parse_existence_for_dialog(gchar * valuestring, GtkWidget * checkbox)
 {
 
 	if (valuestring) {
@@ -215,7 +235,8 @@ void parse_existence_for_dialog(gchar * valuestring, GtkWidget * checkbox)
 	}
 }
 
-void parse_integer_for_dialog(gchar * valuestring, GtkWidget * spin, GtkWidget * entry, GtkWidget * checkbox)
+void
+parse_integer_for_dialog(gchar * valuestring, GtkWidget * spin, GtkWidget * entry, GtkWidget * checkbox)
 {
 
 	gchar *tmp;
@@ -284,9 +305,12 @@ void parse_integer_for_dialog(gchar * valuestring, GtkWidget * spin, GtkWidget *
 /********** DIALOG -> HTML FUNCTIONS *************/
 /*************************************************/
 
-gchar *insert_string_if_string(const gchar *inputstring, gchar * itemname, gchar * string2add2, const gchar *defaultvalue) {
-	gchar *tempstring=NULL;
-	const gchar *value=NULL;
+gchar *
+insert_string_if_string(const gchar * inputstring, gchar * itemname, gchar * string2add2,
+						const gchar * defaultvalue)
+{
+	gchar *tempstring = NULL;
+	const gchar *value = NULL;
 
 	if (!inputstring)
 		value = defaultvalue;
@@ -305,12 +329,13 @@ gchar *insert_string_if_string(const gchar *inputstring, gchar * itemname, gchar
 	return string2add2;
 }
 
-gchar *insert_string_if_entry(GtkEntry* entry, gchar * itemname, gchar * string2add2, gchar *defaultvalue)
+gchar *
+insert_string_if_entry(GtkEntry * entry, gchar * itemname, gchar * string2add2, gchar * defaultvalue)
 {
 	if (entry) {
 		gchar *retstring, *entryvalue;
 		entryvalue = gtk_editable_get_chars(GTK_EDITABLE(entry), 0, -1);
-		if (entryvalue[0]!='\0') {
+		if (entryvalue[0] != '\0') {
 			retstring = insert_string_if_string(entryvalue, itemname, string2add2, defaultvalue);
 			g_free(entryvalue);
 			return retstring;
@@ -320,16 +345,18 @@ gchar *insert_string_if_entry(GtkEntry* entry, gchar * itemname, gchar * string2
 	return string2add2;
 }
 
-gchar *insert_string_if_combobox(GtkComboBox *combobox, gchar * itemname, gchar * string2add2, gchar *defaultvalue) {
+gchar *
+insert_string_if_combobox(GtkComboBox * combobox, gchar * itemname, gchar * string2add2, gchar * defaultvalue)
+{
 	gchar *value, *retstring;
 	if (!combobox)
 		return string2add2;
-	
+
 	value = gtk_combo_box_get_active_text(combobox);
 	if (!value)
 		return string2add2;
-	
-	if (value[0]=='\0') {
+
+	if (value[0] == '\0') {
 		retstring = string2add2;
 	} else {
 		retstring = insert_string_if_string(value, itemname, string2add2, defaultvalue);
@@ -338,7 +365,10 @@ gchar *insert_string_if_combobox(GtkComboBox *combobox, gchar * itemname, gchar 
 	return retstring;
 }
 
-gchar *insert_integer_if_spin(GtkWidget * spin, gchar * itemname, gchar * string2add2, gboolean ispercentage, gint dontinsertonvalue) {
+gchar *
+insert_integer_if_spin(GtkWidget * spin, gchar * itemname, gchar * string2add2, gboolean ispercentage,
+					   gint dontinsertonvalue)
+{
 	gint val = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(spin));
 	if (val != dontinsertonvalue) {
 		gchar *tempstring;
@@ -353,21 +383,25 @@ gchar *insert_integer_if_spin(GtkWidget * spin, gchar * itemname, gchar * string
 	return string2add2;
 }
 
-gchar *insert_attr_if_checkbox(GtkWidget * checkbox, gchar * itemname, gchar *string2add2) {
+gchar *
+insert_attr_if_checkbox(GtkWidget * checkbox, gchar * itemname, gchar * string2add2)
+{
 
 	gchar *tempstring;
-	
+
 	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(checkbox))) {
-		tempstring = g_strdup_printf("%s %s",string2add2, itemname);
+		tempstring = g_strdup_printf("%s %s", string2add2, itemname);
 		g_free(string2add2);
 		string2add2 = tempstring;
 	}
 	return string2add2;
 }
 
-gchar *format_entry_into_string(GtkEntry * entry, gchar * formatstring) {
-	gchar * tempstring;
-	
+gchar *
+format_entry_into_string(GtkEntry * entry, gchar * formatstring)
+{
+	gchar *tempstring;
+
 	if (strlen(gtk_entry_get_text(GTK_ENTRY(entry)))) {
 		tempstring = g_strdup_printf(formatstring, gtk_entry_get_text(GTK_ENTRY(entry)));
 	} else {
@@ -377,7 +411,9 @@ gchar *format_entry_into_string(GtkEntry * entry, gchar * formatstring) {
 
 }
 
-GList *add_entry_to_stringlist(GList *which_list, GtkWidget *entry) {
+GList *
+add_entry_to_stringlist(GList * which_list, GtkWidget * entry)
+{
 	if (entry) {
 		gchar *temp = gtk_editable_get_chars(GTK_EDITABLE(entry), 0, -1);
 		which_list = add_to_stringlist(which_list, temp);
@@ -386,7 +422,9 @@ GList *add_entry_to_stringlist(GList *which_list, GtkWidget *entry) {
 	return which_list;
 }
 
-GtkWidget *generic_table_inside_notebookframe(GtkWidget *notebook, const gchar *title, gint rows, gint cols) {
+GtkWidget *
+generic_table_inside_notebookframe(GtkWidget * notebook, const gchar * title, gint rows, gint cols)
+{
 	GtkWidget *dgtable, *frame = bf_generic_frame_new(NULL, GTK_SHADOW_NONE, 12);
 	gtk_notebook_append_page(GTK_NOTEBOOK(notebook), frame, gtk_label_new(title));
 	dgtable = gtk_table_new(rows, cols, FALSE);
@@ -396,20 +434,31 @@ GtkWidget *generic_table_inside_notebookframe(GtkWidget *notebook, const gchar *
 	return dgtable;
 }
 
-void generic_class_id_style_section(Thtml_diag *dg, gint firstattrwidget, GtkWidget *dgtable, gint firstrowintable, gchar **tagvalues, gint firsttagvalue) {
+void
+generic_class_id_style_section(Thtml_diag * dg, gint firstattrwidget, GtkWidget * dgtable,
+							   gint firstrowintable, gchar ** tagvalues, gint firsttagvalue)
+{
 	GtkWidget *but;
 
-	dg->attrwidget[firstattrwidget] = combobox_with_popdown(tagvalues[firsttagvalue], dg->bfwin->session->classlist, 1);
-	bf_mnemonic_label_tad_with_alignment(_("Cl_ass:"), dg->attrwidget[firstattrwidget], 0, 0.5, dgtable, 0, 1, firstrowintable+0, firstrowintable+1);
-	gtk_table_attach_defaults(GTK_TABLE(dgtable), dg->attrwidget[firstattrwidget], 1, 3, firstrowintable+0, firstrowintable+1);
+	dg->attrwidget[firstattrwidget] =
+		combobox_with_popdown(tagvalues[firsttagvalue], dg->bfwin->session->classlist, 1);
+	bf_mnemonic_label_tad_with_alignment(_("Cl_ass:"), dg->attrwidget[firstattrwidget], 0, 0.5, dgtable, 0, 1,
+										 firstrowintable + 0, firstrowintable + 1);
+	gtk_table_attach_defaults(GTK_TABLE(dgtable), dg->attrwidget[firstattrwidget], 1, 3, firstrowintable + 0,
+							  firstrowintable + 1);
 
-	dg->attrwidget[firstattrwidget+1] = entry_with_text(tagvalues[firsttagvalue+1], 256);
-	bf_mnemonic_label_tad_with_alignment(_("_ID:"), dg->attrwidget[firstattrwidget+1], 0, 0.5, dgtable, 0, 1, firstrowintable+1, firstrowintable+2);
-	gtk_table_attach_defaults(GTK_TABLE(dgtable), dg->attrwidget[firstattrwidget+1], 1, 3, firstrowintable+1, firstrowintable+2);
+	dg->attrwidget[firstattrwidget + 1] = entry_with_text(tagvalues[firsttagvalue + 1], 256);
+	bf_mnemonic_label_tad_with_alignment(_("_ID:"), dg->attrwidget[firstattrwidget + 1], 0, 0.5, dgtable, 0,
+										 1, firstrowintable + 1, firstrowintable + 2);
+	gtk_table_attach_defaults(GTK_TABLE(dgtable), dg->attrwidget[firstattrwidget + 1], 1, 3,
+							  firstrowintable + 1, firstrowintable + 2);
 
-	dg->attrwidget[firstattrwidget+2] = entry_with_text(tagvalues[firsttagvalue+2], 256);
-	bf_mnemonic_label_tad_with_alignment(_("St_yle:"), dg->attrwidget[firstattrwidget+2], 0, 0.5, dgtable, 0, 1, firstrowintable+2, firstrowintable+3);
-	gtk_table_attach_defaults(GTK_TABLE(dgtable), dg->attrwidget[firstattrwidget+2], 1, 2, firstrowintable+2, firstrowintable+3);
-	but = style_but_new(dg->attrwidget[firstattrwidget+2], dg->dialog);
-	gtk_table_attach(GTK_TABLE(dgtable), but, 2, 3, firstrowintable+2, firstrowintable+3, GTK_SHRINK, GTK_SHRINK, 0, 0);
+	dg->attrwidget[firstattrwidget + 2] = entry_with_text(tagvalues[firsttagvalue + 2], 256);
+	bf_mnemonic_label_tad_with_alignment(_("St_yle:"), dg->attrwidget[firstattrwidget + 2], 0, 0.5, dgtable,
+										 0, 1, firstrowintable + 2, firstrowintable + 3);
+	gtk_table_attach_defaults(GTK_TABLE(dgtable), dg->attrwidget[firstattrwidget + 2], 1, 2,
+							  firstrowintable + 2, firstrowintable + 3);
+	but = style_but_new(dg->attrwidget[firstattrwidget + 2], dg->dialog);
+	gtk_table_attach(GTK_TABLE(dgtable), but, 2, 3, firstrowintable + 2, firstrowintable + 3, GTK_SHRINK,
+					 GTK_SHRINK, 0, 0);
 }
