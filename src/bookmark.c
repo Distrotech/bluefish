f5de653e587683706038e51cda41df6261a71a5f
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2011-03-23 04:33:26 +0000

    Bookmark popup menu fixes from TRUNK.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/branches/bluefish_gtk3@6577 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/bookmark.c
+++ src/bookmark.c
@@ -920,23 +920,16 @@ popup_menu(Tbfwin * bfwin, GdkEventButton * event, gboolean show_bmark_specific,
 {
 	GtkWidget *menu = gtk_ui_manager_get_widget(bfwin->uimanager, "/BookmarkMenu");
 
-	if (menu) {
-		if (!show_bmark_specific) {
-			bfwin_action_set_sensitive(bfwin->uimanager, "/BookmarkMenu/Edit", FALSE);
-			bfwin_action_set_sensitive(bfwin->uimanager, "/BookmarkMenu/Delete", FALSE);
-		}
+	g_return_if_fail(menu != NULL);
 
-		if (!show_file_specific)
-			bfwin_action_set_sensitive(bfwin->uimanager, "/BookmarkMenu/DeleteAllInDoc", FALSE);
+	bfwin_action_set_sensitive(bfwin->uimanager, "/BookmarkMenu/Edit", show_bmark_specific);
+	bfwin_action_set_sensitive(bfwin->uimanager, "/BookmarkMenu/Delete", show_bmark_specific);
+	bfwin_action_set_sensitive(bfwin->uimanager, "/BookmarkMenu/DeleteAllInDoc", show_file_specific);
+	bfwin_set_menu_toggle_item_from_path(bfwin->uimanager, "/BookmarkMenu/DefaultPermanent",
+										 main_v->globses.bookmarks_default_store);
 
-		bfwin_set_menu_toggle_item_from_path(bfwin->uimanager, "/BookmarkMenu/DefaultPermanent",
-											 main_v->globses.bookmarks_default_store);
-
-		gtk_widget_show(menu);
-		gtk_menu_popup(GTK_MENU(menu), NULL, NULL, NULL, NULL, event->button, event->time);
-	} else
-		g_warning("showing bookmark popup menu failed");
-
+	gtk_widget_show(menu);
+	gtk_menu_popup(GTK_MENU(menu), NULL, NULL, NULL, NULL, event->button, event->time);
 }
 
 static void
