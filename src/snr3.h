47da02984606b6271db648790ecc52525da314c3
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2011-04-15 15:29:27 +0000

    search in the idle loop now. working for single document search, not yet for multiple document search
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@6640 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/snr3.h
+++ src/snr3.h
@@ -29,6 +29,23 @@ typedef struct {
 	gpointer extra; /* for submatches and such */
 } Tsnr3result;
 
+typedef enum {
+	alldocs,
+	singledocupdate
+} Tsnr3workmode;
+
+typedef struct {
+	gpointer querydata; /* for example the compiled pcre pattern */
+	Tdocument *curdoc;
+	gchar *curbuf;
+	guint so;
+	guint eo;
+	guint curoffset;	
+	Tsnr3workmode workmode;
+	/*void *(*callback) ();*/
+	GCallback callback;
+} Tsnr3working;
+
 typedef struct {
 	Tbfwin *bfwin;
 	gchar *query;
@@ -40,16 +57,19 @@ typedef struct {
 	gboolean escape_chars;
 	gboolean select_matches;
 	gboolean bookmark_matches;
-	
+	Tsnr3working *working;
+	guint idle_id;
 	GQueue results;
 	GList *current;
 	gboolean want_submatches;
 } Tsnr3run;
 
+
 typedef struct {
-	
-	
+	gpointer nothing;
 } Tsnr3config;
+
+#define MAX_CONTINUOUS_SEARCH_INTERVAL 0.1
 
 void snr3_run_go(Tsnr3run *s3run, gboolean forward);
 void snr3run_free(Tsnr3run *s3run);
