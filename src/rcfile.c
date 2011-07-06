b9e9723b09e8d4f468c44d68813127f99bea4065
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2011-07-06 10:08:37 +0000

    added auto close brackets feature
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@6740 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/rcfile.c
+++ src/rcfile.c
@@ -408,6 +408,7 @@ props_init_main(GHashTable * config_rc)
 	init_prop_string(&config_rc, &main_v->props.editor_font_string, "editor_font_string:", "monospace 10");
 	init_prop_integer(&config_rc, &main_v->props.editor_smart_cursor, "editor_smart_cursor:", 1, TRUE);
 	init_prop_integer(&config_rc, &main_v->props.editor_tab_indent_sel, "editor_tab_indent_sel:", 0, TRUE);
+	init_prop_integer(&config_rc, &main_v->props.editor_auto_close_brackets, "editor_auto_close_brackets:", 1, TRUE);
 	init_prop_integer(&config_rc, &main_v->props.use_system_tab_font, "use_system_tab_font:", 1, TRUE);
 	init_prop_string(&config_rc, &main_v->props.tab_font_string, "tab_font_string:", "");
 	init_prop_string(&config_rc, &main_v->props.tab_color_modified, "tab_color_modified:", "#0000FF");
