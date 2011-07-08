d0c04fba67e6649065880611b569dd8f3eaa2b0a
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2011-07-08 06:51:10 +0000

    fix https://bugzilla.gnome.org/show_bug.cgi?id=654155
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@6746 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/bfwin_uimanager.c
+++ src/bfwin_uimanager.c
@@ -1041,6 +1041,10 @@ bfwin_main_menu_init(Tbfwin * bfwin, GtkWidget * vbox)
 	bfwin_recent_menu_create(bfwin);
 
 	set_project_menu_actions(bfwin, FALSE);
+	
+	bfwin_set_menu_toggle_item_from_path(bfwin->uimanager, "/MainMenu/ViewMenu/ViewSidePane", bfwin->session->view_left_panel);
+	bfwin_set_menu_toggle_item_from_path(bfwin->uimanager, "/MainMenu/ViewMenu/ViewMainToolbar", bfwin->session->view_main_toolbar);
+	bfwin_set_menu_toggle_item_from_path(bfwin->uimanager, "/MainMenu/ViewMenu/ViewStatusBar", bfwin->session->view_statusbar);
 }
 
 void
