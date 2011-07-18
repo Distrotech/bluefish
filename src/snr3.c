df1512cf7c6fe636c209ad4248cfe841499e4ff3
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2011-07-18 17:56:11 +0000

    make search next find the next compared to the cursor position if there was no previous search yet
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@6755 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/snr3.c
+++ src/snr3.c
@@ -439,6 +439,56 @@ snr3_run_in_doc(Tsnr3run *s3run, Tdocument *doc, gint so, gint eo) {
 	queue_push(&s3run->idlequeue, rii);
 }
 
+void
+snr3_run_go(Tsnr3run *s3run, gboolean forward) {
+	GList *next=NULL;
+	DEBUG_MSG("snr3_run_go, s3run=%p\n",s3run);
+	if (s3run->current) {
+	DEBUG_MSG("snr3_run_go, s3run->current=%p\n",s3run->current);
+		if (forward) {
+			next = g_list_next(s3run->current);
+		} else {
+			next = g_list_previous(s3run->current);
+		}
+	}
+	if (!next) {
+		g_print("no 'next' (current=%p)\n", s3run->current);
+		next = forward ? s3run->results.head : s3run->results.tail;
+		DEBUG_MSG("snr3_run_go, no next (current=%p), new next=%p\n",s3run->current,next);
+	}
+	if (!s3run->current) {
+		GtkTextBuffer *buffer;
+		GtkTextIter iter;
+		GList *tmplist;
+		gint cursorpos;
+		/* use the cursor position to find the next or previous */
+		buffer = BLUEFISH_TEXT_VIEW(s3run->bfwin->current_document->view)->buffer;
+		gtk_text_buffer_get_iter_at_mark(buffer,&iter, gtk_text_buffer_get_insert(buffer));
+		cursorpos = gtk_text_iter_get_offset(&iter);
+		g_print("cursorpos at %d\n",cursorpos);
+		for (tmplist=g_list_first(s3run->results.head);tmplist;tmplist=g_list_next(tmplist)) {
+			if (((Tsnr3result *)tmplist->data)->doc == s3run->bfwin->current_document) {
+				if ((((Tsnr3result *)tmplist->data)->so == cursorpos)) {
+					next = tmplist;
+					break;
+				} else if ((((Tsnr3result *)tmplist->data)->so < cursorpos)) {
+					if (!forward)
+						next = tmplist;
+				} else if ((((Tsnr3result *)tmplist->data)->so > cursorpos)) {
+					if (forward)
+						next = tmplist;
+						break;
+				}
+			}
+		}
+	}
+	DEBUG_MSG("scroll to result %p\n",next);
+	if (next) {
+		s3run->current = next;
+		scroll_to_result(next->data, NULL);
+	}
+}
+
 void snr3_run_go(Tsnr3run *s3run, gboolean forward) {
 	GList *next=NULL;
 	DEBUG_MSG("snr3_run_go, s3run=%p\n",s3run);
