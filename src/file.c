4472b9ab4d78062b84ebc717326141080826f465
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2011-08-26 13:59:50 +0000

    improve file checkNsave cancelling code, I hope this helps https://bugzilla.gnome.org/show_bug.cgi?id=637990
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@6828 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/file.c
+++ src/file.c
@@ -366,7 +366,10 @@ checkNsave_replace_async_lcb(GObject * source_object, GAsyncResult * res, gpoint
 	g_file_replace_contents_finish(cns->uri, res, &etag, &error);
 	if (error) {
 		DEBUG_MSG("checkNsave_replace_async_lcb,error %d: %s\n", error->code, error->message);
-		if (error->code == G_IO_ERROR_WRONG_ETAG) {
+		if (error->code == G_IO_ERROR_CANCELLED) {
+			cns->callback_func(CHECKANDSAVE_ERROR_CANCELLED, error, cns->callback_data);
+			checkNsave_cleanup(cns);
+		} else if (error->code == G_IO_ERROR_WRONG_ETAG) {
 			if (cns->callback_func(CHECKANDSAVE_ERROR_MODIFIED, error, cns->callback_data) == CHECKNSAVE_CONT) {
 				g_file_replace_contents_async(cns->uri, cns->buffer->data, cns->buffer_size, NULL, TRUE,
 											  G_FILE_CREATE_NONE, NULL, checkNsave_replace_async_lcb, cns);
@@ -427,15 +430,28 @@ checkNsave_replace_async_lcb(GObject * source_object, GAsyncResult * res, gpoint
 }
 
 void
-file_checkNsave_cancel(gpointer cns)
+file_checkNsave_cancel(gpointer data)
 {
-	g_cancellable_cancel(((TcheckNsave *) cns)->cancelab);
+	TcheckNsave *cns = data;
+	if (!cns)
+		return;
+	/* if the checkNsave is still on the queue, and not yet started, the cancellable is NULL
+	and we should remove it from the queue  */
+	if (cns->cancelab) {
+		g_cancellable_cancel(cns->cancelab);
+		return;
+	}
+	queue_remove(&sfqueue, cns);
+	cns->callback_func(CHECKANDSAVE_ERROR_CANCELLED, NULL, cns->callback_data);
+	checkNsave_cleanup(cns);
+	return;
 }
 
 static void
 file_checkNsave_run(gpointer data)
 {
 	TcheckNsave *cns = data;
+	cns->cancelab = g_cancellable_new();
 	g_file_replace_contents_async(cns->uri, cns->buffer->data, cns->buffer_size, cns->etag, cns->backup,
 								  G_FILE_CREATE_NONE, cns->cancelab, checkNsave_replace_async_lcb, cns);
 }
@@ -447,7 +463,6 @@ file_checkNsave_uri_async(GFile * uri, GFileInfo * info, Trefcpointer * buffer, 
 {
 	TcheckNsave *cns;
 	cns = g_slice_new0(TcheckNsave);
-	cns->cancelab = g_cancellable_new();
 	/*cns->etag=NULL; */
 	cns->callback_data = callback_data;
 	cns->callback_func = callback_func;
