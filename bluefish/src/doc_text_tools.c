371ae8d7ae04c6d5eb734f8b8a936475e1622962
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2011-09-15 12:15:23 +0000

    fixes a segfault in the scanner, improves select block and some minor performance enhancements
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@6876 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/doc_text_tools.c
+++ bluefish/src/doc_text_tools.c
@@ -419,6 +419,7 @@ select_between_matching_block_boundaries(Tdocument *doc)
 	GtkTextIter so,eo;
 	static gboolean innerblock=FALSE;
 	GtkTextIter cursor;
+	guint offset;
 	/* if we do not have a selection we start with innerblock is true, in all other
 	cases we just revert innerblock */
 	if (!gtk_text_buffer_get_has_selection(doc->buffer)) {
@@ -429,9 +430,17 @@ select_between_matching_block_boundaries(Tdocument *doc)
 	
 	gtk_text_buffer_get_iter_at_mark(doc->buffer, &cursor, gtk_text_buffer_get_insert(doc->buffer));
 	DEBUG_MSG("select_between_matching_block_boundaries, innerblock=%d, location=%d\n", innerblock, gtk_text_iter_get_offset(&cursor));
+	offset = gtk_text_iter_get_offset(&cursor);
 	if (!bluefish_text_view_get_active_block_boundaries(BLUEFISH_TEXT_VIEW(doc->view), 
-					gtk_text_iter_get_offset(&cursor), innerblock, &so, &eo)) {
+					offset, innerblock, &so, &eo)) {
 		return;
+	}
+	if (innerblock && gtk_text_iter_equal(&so, &eo)) {
+		innerblock = FALSE;
+		if (!bluefish_text_view_get_active_block_boundaries(BLUEFISH_TEXT_VIEW(doc->view), 
+						offset, innerblock, &so, &eo)) {
+			return;
+		}	
 	}
 	gtk_text_buffer_select_range(doc->buffer, &so, &eo);
 }
