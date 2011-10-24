2e7d20df381e7a2cdfa63dd862149b45a833a0e7
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2011-10-24 22:04:21 +0000

    remove debuginfo in snippets plugin
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@6951 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/plugin_snippets/snippetsmenu.c
+++ bluefish/src/plugin_snippets/snippetsmenu.c
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#define DEBUG
+/*#define DEBUG*/
 #include <gtk/gtk.h>
 #include "../bluefish.h"
 #include "snippetsmenu.h"
