f5714ce0093046b1e0205d8df8b1aa8ae7fde7b7
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2011-12-31 11:38:14 +0000

    fix the session saving of search and replace, especially the recusrion_level
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@7167 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/rcfile.c
+++ bluefish/src/rcfile.c
@@ -933,12 +933,13 @@ return_session_configlist(GHashTable * configlist, Tsessionvars * session)
 	init_prop_integer(&configlist, &session->autocomplete, "autocomplete:", 1, FALSE);
 	init_prop_integer(&configlist, &session->show_mbhl, "show_mbhl:", 1, FALSE);
 	
-	init_prop_integer(&configlist, &session->snr3_type, "snr_type:", 1, 0);
-	init_prop_integer(&configlist, &session->snr3_replacetype, "snr_replacetype:", 1, 0);
-	init_prop_integer(&configlist, &session->snr3_scope, "snr_scope:", 1, 0);
-	init_prop_integer(&configlist, &session->snr3_casesens, "snr_casesens:", 1, 0);
-	init_prop_integer(&configlist, &session->snr3_escape_chars, "snr_escape_chars:", 1, 0);
-	init_prop_integer(&configlist, &session->snr3_dotmatchall, "snr_dotmatchall:", 1, 1);
+	init_prop_integer(&configlist, &session->snr3_type, "snr_type:", 1, FALSE);
+	init_prop_integer(&configlist, &session->snr3_replacetype, "snr_replacetype:", 1, FALSE);
+	init_prop_integer(&configlist, &session->snr3_scope, "snr_scope:", 1, FALSE);
+	init_prop_integer(&configlist, &session->snr3_casesens, "snr_casesens:", 1, FALSE);
+	init_prop_integer(&configlist, &session->snr3_escape_chars, "snr_escape_chars:", 1, FALSE);
+	init_prop_integer(&configlist, &session->snr3_dotmatchall, "snr_dotmatchall:", 1, FALSE);
+	init_prop_integer(&configlist, &session->snr3_recursion_level, "snr_recursion_level:", 5, FALSE);
 
 	init_prop_integer(&configlist, &session->display_right_margin, "display_right_margin:", 0, FALSE);
 #ifdef HAVE_LIBENCHANT
