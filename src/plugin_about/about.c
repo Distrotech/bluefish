6db8052c36d111af5dd52ca204581fca5304143d
dleidert <dleidert@7640d186-872a-4c71-b7b2-a5696852cc35>
2012-04-02 22:35:01 +0000

    Fix copyright statement.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@7338 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/plugin_about/about.c
+++ src/plugin_about/about.c
@@ -216,7 +216,7 @@ about_dialog_create(GtkAction * action, gpointer user_data)
 		NULL
 	};
 
-	const gchar *copyright = "Copyright \xc2\xa9 1998-2010 Olivier Sessink and others.\n";
+	const gchar *copyright = "Copyright \xc2\xa9 1998-2012 Olivier Sessink and others.\n";
 
 	/* wrap the license here,
 	 * the "wrap-license" property is only available with GTK >= 2.8
