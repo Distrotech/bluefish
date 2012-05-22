dcecf518ca2138b188c67331e3a1e15d7816c929
jimh6583 <jimh6583@7640d186-872a-4c71-b7b2-a5696852cc35>
2012-05-22 13:07:10 +0000

    Fix compile warnings.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@7462 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/plugin_htmlbar/html2.h
+++ src/plugin_htmlbar/html2.h
@@ -20,15 +20,16 @@
 
 #ifndef __HTML2_H_
 #define __HTML2_H_
-void insert_class(GtkWidget * widget, gpointer data);
 
 /* void insert_in_stylesheet(GtkWidget * widget, gpointer data); */
 
+void insert_class(GtkWidget * widget, gpointer data);
 GtkWidget *style_but_new(GtkWidget * which_entry, GtkWidget * win);
 GtkWidget *style_but_new_for_wizard(GtkWidget * textview);
 void new_css_dialog(GtkWidget *wid,Tbfwin *bfwin);
 void edit_color_dialog(Tdocument *doc, gchar *color, gint startpos, gint endpos);
 void sel_colour_cb(GtkWidget *widget, Tbfwin *bfwin);
 GtkWidget *color_but_new(GtkWidget * which_entry, GtkWidget * win);
+gchar *return_color(gchar *start_value);
 
 #endif /* __HTML2_H_ */
