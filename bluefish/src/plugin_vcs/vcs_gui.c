e2eeeec60135cd3ab3dc7c70110487698eb518f1
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2012-09-29 20:19:26 +0000

    svn commit is now working
    this commit is done from the bluefish GUI
    
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@7663 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/plugin_vcs/vcs_gui.c
+++ bluefish/src/plugin_vcs/vcs_gui.c
@@ -30,6 +30,50 @@ vcs_gui_update_status(gpointer data, GList *list)
 
 }
 
+static void
+vcs_commit_dialog_lcb(GtkDialog *dialog,gint response,gpointer user_data)
+{
+	Tvcs_gui *vg = user_data;
+	if (response == GTK_RESPONSE_ACCEPT) {
+		GList *list=NULL;
+		GtkTextIter si, ei;
+		GtkTextBuffer *buffer;
+		gchar *message;
+		GtkTreeIter iter;
+		gboolean cont;
+		/*build the list */
+		cont = gtk_tree_model_get_iter_first(GTK_TREE_MODEL(vg->filestore),&iter);
+		while(cont) {
+			gboolean enabled;
+			gtk_tree_model_get(GTK_TREE_MODEL(vg->filestore),&iter,0, &enabled, -1);
+			if (enabled) {
+				gchar *tmpstr;
+				gtk_tree_model_get(GTK_TREE_MODEL(vg->filestore),&iter,1, &tmpstr, -1);
+				list = g_list_prepend(list, tmpstr);
+			}
+			cont = gtk_tree_model_iter_next(GTK_TREE_MODEL(vg->filestore), &iter);
+		}
+		buffer = gtk_text_view_get_buffer(vg->tview);
+		gtk_text_buffer_get_bounds(buffer,&si,&ei);
+		message = gtk_text_buffer_get_text(buffer, &si, &ei, TRUE);
+		vcs_commit_real(vg->bfwin, vg->vs, list, message);
+		g_list_free_full(list, g_free);
+		g_free(message);
+	}
+	gtk_widget_destroy(GTK_WIDGET(dialog));
+	g_slice_free(Tvcs_gui, vg);
+}
+
+static void
+active_edited_lcb(GtkCellRendererToggle * cellrenderertoggle, gchar * path, Tvcs_gui *vg)
+{
+	GtkTreeIter iter;
+	GtkTreePath *tpath;
+	tpath = gtk_tree_path_new_from_string(path);
+	if (tpath && gtk_tree_model_get_iter(GTK_TREE_MODEL(vg->filestore), &iter, tpath)) {
+		gtk_list_store_set(GTK_LIST_STORE(vg->filestore), &iter, 0, !gtk_cell_renderer_toggle_get_active(cellrenderertoggle), -1);
+	}
+}
 
 void
 vcs_commit_dialog(Tbfwin *bfwin, Tvcssession *vs)
@@ -56,6 +100,8 @@ vcs_commit_dialog(Tbfwin *bfwin, Tvcssession *vs)
 	renderer = gtk_cell_renderer_toggle_new();
 	column = gtk_tree_view_column_new_with_attributes(_("Include"), renderer, "active", 0, NULL);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(vg->fileview), column);
+	g_object_set(G_OBJECT(renderer), "activatable", TRUE, NULL);
+	g_signal_connect(G_OBJECT(renderer), "toggled", G_CALLBACK(active_edited_lcb), vg);
 	renderer = gtk_cell_renderer_text_new();
 	column = gtk_tree_view_column_new_with_attributes(_("Filename"), renderer, "text", 1, NULL);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(vg->fileview), column);
@@ -65,7 +111,7 @@ vcs_commit_dialog(Tbfwin *bfwin, Tvcssession *vs)
 	gtk_container_add(GTK_CONTAINER(scrolwin), GTK_WIDGET(vg->fileview));
 
 	gtk_box_pack_start(GTK_BOX(vbox), scrolwin, TRUE, TRUE, 4);
-	gtk_box_pack_start(GTK_BOX(vbox), gtk_vseparator_new(), TRUE, FALSE, 4);
+	gtk_box_pack_start(GTK_BOX(vbox), gtk_vseparator_new(), FALSE, FALSE, 4);
 
 	vg->tview = gtk_text_view_new();
 	scrolwin = gtk_scrolled_window_new(NULL, NULL);
@@ -74,6 +120,7 @@ vcs_commit_dialog(Tbfwin *bfwin, Tvcssession *vs)
 	gtk_box_pack_start(GTK_BOX(vbox), scrolwin, TRUE, TRUE, 4);
 
 	vcs_status(bfwin, vs, vg);
-
+	gtk_window_set_default_size(GTK_WINDOW(vg->win), 300, 300);
 	gtk_widget_show_all(vg->win);
+	g_signal_connect(vg->win, "response", G_CALLBACK(vcs_commit_dialog_lcb), vg);
 }
