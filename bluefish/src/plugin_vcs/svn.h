d8629e5faabf13dd5941613626b4761e1fce7df1
dleidert <dleidert@7640d186-872a-4c71-b7b2-a5696852cc35>
2012-10-21 11:41:40 +0000

    Fix a few compiler warnings.
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@7684 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/plugin_vcs/svn.h
+++ bluefish/src/plugin_vcs/svn.h
@@ -1,3 +1,6 @@
 
-
 void svn_status(Tbfwin *bfwin, const gchar *basedir, gpointer data);
+void svn_commit_real(Tbfwin *bfwin, Tvcssession *vs, GList *files, const gchar *message);
+gboolean svn_autoconfigure(Tvcssession *vs, GFile *cururi);
+void svn_update(Tbfwin *bfwin, const gchar *basedir, gpointer data);
+
