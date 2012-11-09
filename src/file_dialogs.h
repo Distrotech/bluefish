24e0593a47435e406e696e8a0ac100cfec1a3548
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2012-11-09 20:02:45 +0000

    adding 'save a copy', and rewrite some of the code that handles save, save as, rename and save to add copy in a more efficient way
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@7711 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/file_dialogs.h
+++ src/file_dialogs.h
@@ -37,8 +37,16 @@ void files_advanced_win(Tbfwin * bfwin, gchar * basedir);
 void file_open_advanced_cb(GtkWidget * widget, Tbfwin * bfwin);
 void file_open_cb(GtkWidget * widget, Tbfwin * bfwin);
 void file_open_url_cb(GtkAction * action, Tbfwin * bfwin);
-gchar *ask_new_filename(Tbfwin * bfwin, const gchar * old_curi, const gchar * gui_name, gboolean is_move);
-void doc_save_backend(Tdocument * doc, gboolean do_save_as, gboolean do_move, gboolean close_doc,
+gchar *ask_new_filename(Tbfwin * bfwin, const gchar * old_curi, const gchar * dialogtext);
+
+typedef enum {
+	docsave_normal,
+	docsave_saveas,
+	docsave_move,
+	docsave_copy
+} Tdocsave_mode;
+
+void doc_save_backend(Tdocument * doc, Tdocsave_mode savemode, gboolean close_doc,
 					  gboolean close_window);
 void file_save_cb(GtkWidget * widget, Tbfwin * bfwin);
 void file_save_as_cb(GtkWidget * widget, Tbfwin * bfwin);
