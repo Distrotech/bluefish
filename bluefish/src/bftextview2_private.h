f8dd8c47d6217f227c2728692c5c563b2c6e2402
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2013-05-09 08:06:47 +0000

    disable DBG_SCANCACHE because the amount of logging makes testing difficult
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk@7907 7640d186-872a-4c71-b7b2-a5696852cc35

--- bluefish/src/bftextview2_private.h
+++ bluefish/src/bftextview2_private.h
@@ -38,7 +38,7 @@ extern void g_none(char *first, ...);
 /*#define CHECK_CONSISTENCY*/
 
 #define DBG_MSG DBG_NONE
-#define DBG_SCANCACHE g_print
+#define DBG_SCANCACHE DBG_NONE
 #define DBG_SCANNING DBG_NONE
 #define DBG_BLOCKMATCH DBG_NONE
 #define DBG_PATCOMPILE DBG_NONE
