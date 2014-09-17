9fb2497050af066334db7b52f93e729a6f836dfe
oli4 <oli4@7640d186-872a-4c71-b7b2-a5696852cc35>
2014-09-17 06:34:33 +0000

    fixes crash in autocompletion reported in https://bugzilla.gnome.org/show_bug.cgi?id=736010
    git-svn-id: svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish@8378 7640d186-872a-4c71-b7b2-a5696852cc35

--- src/bftextview2_patcompile.c
+++ src/bftextview2_patcompile.c
@@ -826,7 +826,7 @@ match_add_autocomp_item(Tscantable * st, guint16 matchnum, const gchar * autocom
 {
 	Tpattern_autocomplete *pac = g_slice_new(Tpattern_autocomplete);
 	if (autocomplete_string) {
-		pac->autocomplete_string = (gchar *) autocomplete_string;
+		pac->autocomplete_string = g_strdup(autocomplete_string);
 	} else if (autocomplete_append) {
 		pac->autocomplete_string =
 			g_strconcat(g_array_index(st->matches, Tpattern, matchnum).pattern, autocomplete_append, NULL);
